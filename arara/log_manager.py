3cfcc714f1e9451f2c54c188f62dc01d168c798c
mikkang <devnull@localhost>
2008-06-24 01:38:56 +0000

    로그관리 클래스 작성(미완성..짜증날정도로 putty가 자꾸 죽어서..ㅠ)

--- arara/log_manager.py
+++ arara/log_manager.py
@@ -2,4 +2,37 @@
 
 class LogManager(object):
     """
-   
+    로그를 관리하는 클래스
+    """
+
+    def __init___(self):
+	pass
+
+    def add_log(self, log_dic):
+	"""
+	로그를 추가하는 함수
+	
+	>>> log.add_log(log_dic)
+	True, "OK"
+
+	@type  log_dic: dictionary
+	@param log_dic: Log Dictionary
+	@rtype: string
+	@return: True, "OK"
+	"""
+
+    def remove_log(self, log_dic):
+	"""
+
+
+	로그를 추가하는 함수
+	
+	>>> log.add_log(log_dic)
+	True, "OK"
+
+	@type  log_dic: dictionary
+	@param log_dic: Log Dictionary
+	@rtype: string
+	@return: True, "OK"
+	"""
+
