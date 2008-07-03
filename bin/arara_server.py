33da94e677719a265028ea89f011211378573f40
serialx <devnull@localhost>
2008-07-03 06:05:11 +0000

    ARAra XMLRPC 서버 port 바인딩 되도록 수정

--- bin/arara_server.py
+++ bin/arara_server.py
@@ -2,8 +2,11 @@
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import os
 import sys
+import optparse
+
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.append(PROJECT_PATH)
+
 from arara.article_manager import ArticleManager
 from arara.blacklist_manager import BlacklistManager
 from arara.login_manager import LoginManager
@@ -13,9 +16,16 @@ class Namespace(object):
     article_manager = ArticleManager(login_manager)
     blacklist_manager = BlacklistManager()
 
-server = SimpleXMLRPCServer(("",4949))
-server.register_introspection_functions()
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option("-p", "--port=", dest="port", default=8000, type="int",
+                      help="The port to bind")
+    options, args = parser.parse_args()
+    print "Opening ARAra XMLRPC middleware on port", options.port, "..."
 
-server.register_instance(Namespace(), allow_dotted_names=True)
+    server = SimpleXMLRPCServer(("", options.port))
+    server.register_introspection_functions()
 
-server.serve_forever()
+    server.register_instance(Namespace(), allow_dotted_names=True)
+
+    server.serve_forever()
