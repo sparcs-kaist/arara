96b310e5eadc80df6ab0061000f4784a799a1e22
serialx <devnull@localhost>
2008-07-03 05:37:37 +0000

    XMLRPC 서버를 /bin 디렉토리로 옮김.

---
+++ bin/xml_rpc_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import xmlrpclib
+
+s = xmlrpclib.Server('http://localhost:4949')
+test= s.login_manager.login('test', 'test', '143.248.234.145')
+
+print s.article_manager.write_article(test[1], 'garbages', {'title':'123', 'cont':'234'})
+print s.article_manager.write_reply(test[1], 'garbages', {'context':'222'}, 1)
+print s.article_manager.read(test[1], 'garbages', 1)
+
