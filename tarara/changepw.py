86fa5c5f324a0da1e4a01c976acc388bc9738d30
peremen <devnull@localhost>
2008-07-08 05:12:42 +0000

    Add change password screen

--- tarara/changepw.py
+++ tarara/changepw.py
@@ -23,29 +23,21 @@ class ara_join(object):
         dash = urwid.SolidFill(utf8decode('─'))
         blank = urwid.SolidFill(u" ")
         blanktext = urwid.Filler(urwid.Text(' '))
-	header = urwid.Filler(urwid.Text("ARA: Join", align='center'))
+	header = urwid.Filler(urwid.Text("ARA: Change Password", align='center'))
 
-        idedit = urwid.Filler(urwid.Edit(caption="ID:", wrap='clip'))
-        iddesc = urwid.Filler(urwid.Text("ID's length should be\nbetween 4 and 10 chars"))
-        idcolumn = self._make_column(idedit, iddesc)
+        oldpwedit = urwid.Filler(urwid.Edit(caption="ID:", wrap='clip'))
+        oldpwdesc = urwid.Filler(urwid.Text("Please enter your\nold password"))
+        oldpwcolumn = self._make_column(oldpwedit, oldpwdesc)
 
-        nickedit = urwid.Filler(urwid.Edit(caption="Nickname:", wrap='clip'))
-        nickdesc=urwid.Filler(urwid.Text("You can't use duplicated\nnickname or other's ID"))
-        nickcolumn = self._make_column(nickedit, nickdesc)
-        
-        pwedit = urwid.Filler(urwid.Edit(caption="Password:", wrap='clip'))
-        pwdesc = urwid.Filler(urwid.Text("Minimum password length\nis 4 characters"))
-        pwcolumn = self._make_column(pwedit, pwdesc)
+        newpwedit = urwid.Filler(urwid.Edit(caption="Password:", wrap='clip'))
+        newpwdesc = urwid.Filler(urwid.Text("Minimum password length\nis 4 characters"))
+        newpwcolumn = self._make_column(newpwedit, newpwdesc)
 
-        confirmedit = urwid.Filler(urwid.Edit(caption="Confirm\nPassword:", wrap='clip'))
-        confirmdesc = urwid.Filler(urwid.Text("Type password here\nonce again"))
+        confirmedit = urwid.Filler(urwid.Edit(caption="Confirm\nnew password:", wrap='clip'))
+        confirmdesc = urwid.Filler(urwid.Text("Re-enter your new\npassword"))
         confirmcolumn = self._make_column(confirmedit, confirmdesc)
 
-        emailedit = urwid.Filler(urwid.Edit(caption="E-Mail:", wrap='clip'))
-        emaildesc = urwid.Filler(urwid.Text("We'll send confirmation\nmail to this address"))
-        emailcolumn = self._make_column(emailedit, emaildesc)
-
-        self.joinpile = urwid.Pile([idcolumn, nickcolumn,pwcolumn,confirmcolumn,emailcolumn])
+        self.joinpile = urwid.Pile([oldpwcolumn, newpwcolumn,confirmcolumn])
 
         joinbutton = urwid.Filler(urwid.Button("Join"))
         cancelbutton = urwid.Filler(urwid.Button("Cancel"))
@@ -55,11 +47,9 @@ class ara_join(object):
         self.langlist = urwid.LineBox(urwid.ListBox(urwid.SimpleListWalker(langitems)))
 
         infotext = urwid.Filler(urwid.Text("""  * Press [Enter] to proceed to the next item, [Shift+Enter] - previous item
-  * Press [Tab] to directly jump to Join or Cancel button
-  * We'll send confirmation mail to your address.
-  * To activate your ID, click the link on the mail."""))
+  * Press [Tab] to directly jump to OK or Cancel button"""))
 
-        content = [('fixed',1,header),self.joinpile,('fixed',4,infotext),('fixed',1,blank),('fixed',1,buttoncolumn)]
+        content = [('fixed',1,header),self.joinpile,('fixed',2,infotext),('fixed',1,blank),('fixed',1,buttoncolumn)]
         self.mainpile = urwid.Pile(content)
 
         self.frame = self.mainpile
