642fdebd9c65376721d194afc2e6f303f3c7c276
peremen <devnull@localhost>
2008-07-08 05:01:19 +0000

    Add sig&intro edit screen. Remove unneeded functions.

--- tarara/sig_intro.py
+++ tarara/sig_intro.py
@@ -11,8 +11,6 @@ keymap = {
 }
 
 class ara_post(object):
-    def get_current_board(self):
-	return "garbages"
 
     def __init__(self):
         utf8decode = urwid.escape.utf8decode
@@ -20,21 +18,19 @@ class ara_post(object):
         blank = urwid.SolidFill(u" ")
         blanktext = urwid.Filler(urwid.Text(' '))
 
-	self.header = urwid.Filler(urwid.Text(u"ARA: Post Article  Current board: %s" % self.get_current_board(), align='center'))
+	self.header = urwid.Filler(urwid.Text(u"ARA: Change Introduction & Signature", align='center'))
 
-        titleedit = urwid.Filler(urwid.Edit(caption="Title: ", wrap='clip'))
-        bodytext = urwid.Filler(urwid.Text('Body'))
-        self.bodyedit = urwid.Filler(urwid.Edit(multiline = True, wrap='clip'))
+        sigtext = urwid.Filler(urwid.Text('Signature'))
+        sigedit = urwid.Filler(urwid.Edit(wrap='clip'))
+        introtext = urwid.Filler(urwid.Text('Introduction'))
+        introedit = urwid.Filler(urwid.Edit(wrap='clip'))
 
-	self.chkinclude = urwid.CheckBox("Include in search")
-	self.btnhelp = urwid.Button("Help")
-	self.btnpreview = urwid.Button("Preview")
 	self.btnokay = urwid.Button("OK")
 	self.btncancel = urwid.Button("Cancel")
 
-        self.bottomcolumn = urwid.Filler(urwid.Columns([('weight',40,self.chkinclude),('weight',15,self.btnhelp),('weight',15,self.btnpreview),('weight',15,self.btnokay),('weight',15,self.btncancel)]))
+        self.bottomcolumn = urwid.Filler(urwid.Columns([self.btnokay,self.btncancel]))
 
-        content = [('fixed',1, self.header),('fixed',1,titleedit),('fixed',1,bodytext),self.bodyedit,('fixed',1,self.bottomcolumn)]
+        content = [('fixed',1, self.header),('fixed',1,sigtext),sigedit,('fixed',1,introtext),introedit,('fixed',1,self.bottomcolumn)]
         self.mainpile = urwid.Pile(content)
 
         self.frame = self.mainpile
