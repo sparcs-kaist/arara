ae2a6339a30844bccc0f293c41618e111c547617
serialx <devnull@localhost>
2008-07-08 07:58:22 +0000

    messaging_manr 테스트 임시 추가 및 namespace 객체 싱글톤으로 바꿈.

--- arara/__init__.py
+++ arara/__init__.py
@@ -1,2 +1,24 @@
+from arara.article_manager import ArticleManager
+from arara.blacklist_manager import BlacklistManager
+from arara.member_manager import MemberManager
+from arara.login_manager import LoginManager
+
+
+class Namespace(object):
+    def __init__(self):
+        self.login_manager = LoginManager()
+        self.member_manager = MemberManager()
+        self.login_manager._set_member_manager(self.member_manager)
+        self.member_manager._set_login_manager(self.login_manager)
+        self.article_manager = ArticleManager(self.login_manager)
+        self.blacklist_manager = BlacklistManager()
+
+namespace = None
+
+def get_namespace():
+    global namespace
+    if not namespace:
+        namespace = Namespace()
+    return namespace
 
 # vim: set et ts=8 sw=4 sts=4
