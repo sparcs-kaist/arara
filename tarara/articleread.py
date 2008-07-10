af1417cac1313e76859afa7a22653ef1febf0984
peremen <devnull@localhost>
2008-07-10 11:13:11 +0000

    일부 폼을 ara_forms를 사용하도록 수정. 앗싸 140은 내꺼

--- tarara/articleread.py
+++ tarara/articleread.py
@@ -4,53 +4,26 @@
 import os
 import urwid.curses_display
 import urwid
+from common import *
 
-keymap = {
-    'j': 'down',
-    'k': 'up',
-}
-
-class ara_article_read(object):
+class ara_article_read(ara_forms):
     def get_current_board(self):
 	return "garbages"
 
-    def __init__(self):
-        utf8decode = urwid.escape.utf8decode
-        dash = urwid.SolidFill(utf8decode('─'))
-        blank = urwid.SolidFill(u" ")
-        blanktext = urwid.Filler(urwid.Text(' '))
-
+    def __initwidgets__(self):
+        self.keymap = {
+            'j': 'down',
+            'k': 'up',
+        }
 	self.header = urwid.Filler(urwid.Text(u"ARA: Read article",align='center'))
         functext = urwid.Filler(urwid.Text('(n)ext/(p)revious (b)lock (e)dit (d)elete (f)old/retract (r)eply (h)elp (q)uit'))
 	titletext = urwid.Filler(urwid.Text('Title: %s' % "윅베간다"))
 	infotext = urwid.Filler(urwid.Text('Author: %(id)s (%(nickname)s)    Hit: %(hit)s Reply: %(reply)s %(date)s' % {'id':'peremen', 'nickname':'peremen','hit':500,'reply':'10','date':'today'}))
 
-        content = [('fixed',1, self.header),('fixed',1,functext),('fixed',1,titletext),('fixed',1,infotext),('fixed',1,dash),blanktext]
+        content = [('fixed',1, self.header),('fixed',1,functext),('fixed',1,titletext),('fixed',1,infotext),('fixed',1,self.dash),self.blanktext]
         self.mainpile = urwid.Pile(content)
 
-        self.frame = self.mainpile
-
-    def main(self):
-        self.ui = urwid.curses_display.Screen()
-        self.ui.run_wrapper(self.run)
-
-    def run(self):
-        size = self.ui.get_cols_rows()
-        quit = False
-        while not quit:
-            self.draw_screen(size)
-            keys = self.ui.get_input()
-            for key in keys:
-                if key == 'tab':
-                    quit = True
-                    break
-                if key in keymap:
-                    key = keymap[key]
-                self.frame.keypress(size, key)
-   
-    def draw_screen(self, size):
-        canvas = self.frame.render(size, focus=True)
-        self.ui.draw_screen(size, canvas)
+        return self.mainpile
 
 ara_article_read().main()
 
--- tarara/login.py
+++ tarara/login.py
@@ -5,24 +5,14 @@ import os
 import urwid.curses_display
 import urwid
 
-keymap = {
-    'j': 'down',
-    'k': 'up',
-}
-
-class ara_login(object):
+class ara_login(ara_forms):
     def get_login_message(self):
         basedir = os.path.dirname(__file__)
         banner = os.path.join(basedir, 'login.txt')
         f = open(banner, 'r')
         return f.read().decode('utf-8')
 
-    def __init__(self):
-        utf8decode = urwid.escape.utf8decode
-        dash = urwid.SolidFill(utf8decode('─'))
-        blank = urwid.SolidFill(u" ")
-        blanktext = urwid.Filler(urwid.Text(' '))
-
+    def __initwidgets__(self):
         self.message = urwid.Filler(urwid.Text(self.get_login_message(), align="center"))
         self.message_ko = urwid.Filler(urwid.Text(u"[Tab] 키를 누르면 항목간을 전환할 수 있습니다", align='center'))
         self.message_en = urwid.Filler(urwid.Text(u"Press [Tab] key to jump between each items", align='center'))
@@ -43,29 +33,8 @@ class ara_login(object):
         content = [self.message,('fixed',1, dash), ("fixed", 1, self.message_ko), ('fixed',1,self.message_en), ('fixed',1,blank), ('fixed',4,self.bottomcolumn)]
         self.mainpile = urwid.Pile(content)
 
-        self.frame = self.mainpile
+	return self.mainpile
 
-    def main(self):
-        self.ui = urwid.curses_display.Screen()
-        self.ui.run_wrapper(self.run)
-
-    def run(self):
-        size = self.ui.get_cols_rows()
-        quit = False
-        while not quit:
-            self.draw_screen(size)
-            keys = self.ui.get_input()
-            for key in keys:
-                if key == 'tab':
-                    quit = True
-                    break
-                if key in keymap:
-                    key = keymap[key]
-                self.frame.keypress(size, key)
-   
-    def draw_screen(self, size):
-        canvas = self.frame.render(size, focus=True)
-        self.ui.draw_screen(size, canvas)
 
 ara_login().main()
 
