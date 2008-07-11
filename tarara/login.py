346dfc69589f0be2f65ebc34acbc8b86b0f766a9
peremen <devnull@localhost>
2008-07-11 07:53:52 +0000

    Communication with XML RPC Server

--- tarara/login.py
+++ tarara/login.py
@@ -12,25 +12,49 @@ class ara_login(ara_forms):
         banner = os.path.join(basedir, 'login.txt')
         f = open(banner, 'r')
         return f.read().decode('utf-8')
+    
+    def login(self, id, password, ip):
+        retvalue = self.server.login_manager.login(id, password, ip)
+        if retvalue[0] == True:
+            return
+        else:
+            if retvalue[1] == 'WRONG_ID':
+                self.errormessage.body.set_text(u"ID가 ㅇ벗습니다.")
+            elif retvalue[1] == 'WRONG_PASSWORD':
+                self.errormessage.body.set_text(u"비밀번호가 틀렸습니다.")
+            elif retvalue[1] == 'DATABASE_ERROR':
+                self.errormessage.body.set_text(u"데이터베이스 오류가 발생했습니다.")
+            else:
+                assert(False)
 
     def __keypress__(self, size, key):
-        self.langlist.get_w().set_focus(1)
+        curfocus = self.bottomcolumn.get_focus_column()
         if key.strip() == 'tab':
-            curfocus = self.bottomcolumn.get_focus_column()
             self.bottomcolumn.set_focus_column((curfocus + 1) % 3)
         elif key.strip() == 'enter':
-            pass
+            if curfocus == 0:
+                if self.idpwpile.get_focus() == self.idedit:
+                    self.idpwpile.set_focus(1)
+                elif self.idpwpile.get_focus() == self.pwedit:
+                    self.login(self.idedit.body.get_edit_text(), self.pwedit.body.get_edit_text(), "127.0.0.1")
+                else:
+                    assert(False)
+            elif curfocus == 1:
+                return
+            elif curfocus == 2:
+                return
         else:
             self.frame.keypress(size, key)
 
     def __initwidgets__(self):
         self.message = urwid.Filler(urwid.Text(self.get_login_message(), align="center"))
+        self.errormessage = urwid.Filler(urwid.Text("", align="center"))
         self.message_ko = urwid.Filler(urwid.Text(u"[Tab] 키를 누르면 항목간을 전환할 수 있습니다", align='center'))
         self.message_en = urwid.Filler(urwid.Text(u"Press [Tab] key to jump between each items", align='center'))
 
-        idedit = urwid.Filler(urwid.Edit(caption="ID:", wrap='clip'))
-        pwedit = urwid.Filler(urwid.Edit(caption="Password:", wrap='clip'))
-        self.idpwpile = urwid.Pile([idedit, pwedit])
+        self.idedit = urwid.Filler(urwid.Edit(caption="ID:", wrap='clip'))
+        self.pwedit = urwid.Filler(urwid.Edit(caption="Password:", wrap='clip'))
+        self.idpwpile = urwid.Pile([self.idedit, self.pwedit])
 
         langitems = [urwid.Text('Korean'), urwid.Text('English'), urwid.Text('Chinese')]
         self.langlist = urwid.LineBox(urwid.ListBox(urwid.SimpleListWalker(langitems)))
@@ -40,7 +64,7 @@ class ara_login(ara_forms):
 
         self.bottomcolumn = urwid.Columns([('weight',40,self.idpwpile),('weight',30,self.langlist),('weight',30,self.joinlist)])
 
-        content = [self.message,('fixed',1, self.dash), ("fixed", 1, self.message_ko), ('fixed',1,self.message_en), ('fixed',1,self.blank), ('fixed',4,self.bottomcolumn)]
+        content = [self.message,('fixed',1,self.errormessage),('fixed',1, self.dash), ("fixed", 1, self.message_ko), ('fixed',1,self.message_en), ('fixed',1,self.blank), ('fixed',4,self.bottomcolumn)]
         self.mainpile = urwid.Pile(content)
 
         self.keymap = {'left':'', 'right':''}
