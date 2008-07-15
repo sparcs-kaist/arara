0b81470e1bfdf9df0b8759102f5119d5580983fb
peremen <devnull@localhost>
2008-07-15 11:50:14 +0000

    마지막 UI도 일단 완성.

---
+++ tarara/blacklist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import os
+import urwid.curses_display
+import urwid
+from ara_forms import *
+from widget import *
+
+class ara_blacklist(ara_forms):
+    def __initwidgets__(self):
+	self.header = urwid.Filler(urwid.Text(u"ARA: My Blacklist",align='center'))
+
+        okbutton = urwid.Filler(urwid.Button("OK"))
+        cancelbutton = urwid.Filler(urwid.Button("Cancel"))
+        buttoncolumn = self._make_column(okbutton, cancelbutton, 50, 50)
+
+        content = [('fixed',1, self.header),('fixed',1,self.dash),self.blanktext, ('fixed',1,self.dash),('fixed',1,buttoncolumn)]
+        self.mainpile = urwid.Pile(content)
+
+        return self.mainpile
+
+if __name__=="__main__":
+    ara_blacklist(session_key = "e00bc932cc2d375075f443133ae0fa44").main()
+
+# vim: set et ts=8 sw=4 sts=4:
---
+++ tarara/list_allusers.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import os
+import urwid.curses_display
+import urwid
+from ara_forms import *
+from widget import *
+
+class ara_list_allusers(ara_forms):
+    def __initwidgets__(self):
+	self.header = urwid.Filler(urwid.Text(u"ARA: List all users",align='center'))
+        self.querytext = urwid.Filler(urwid.Edit(caption=" * Search: ", wrap='clip'))
+        self.infotext = urwid.Filler(urwid.Text("(q)uit (Enter) query"))
+
+        okbutton = urwid.Filler(urwid.Button("OK"))
+        cancelbutton = urwid.Filler(urwid.Button("Cancel"))
+        buttoncolumn = self._make_column(okbutton, cancelbutton, 50, 50)
+
+        content = [('fixed',1, self.header),('fixed',1,self.querytext),('fixed',1,self.infotext),('fixed',1,self.dash),self.blanktext, ]
+        self.mainpile = urwid.Pile(content)
+
+        return self.mainpile
+
+if __name__=="__main__":
+    ara_list_allusers(session_key = "e00bc932cc2d375075f443133ae0fa44").main()
+
+# vim: set et ts=8 sw=4 sts=4:
