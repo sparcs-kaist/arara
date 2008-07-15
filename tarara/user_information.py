1102edbf424a5e14a5c0cbc981d495cc35259a17
peremen <devnull@localhost>
2008-07-15 11:35:44 +0000

    User information is now working

--- tarara/user_information.py
+++ tarara/user_information.py
@@ -5,7 +5,7 @@ import os
 import urwid.curses_display
 import urwid
 from widget import *
-from common import *
+from ara_forms import *
 
 class ara_user_information(ara_forms):
     menu = [
@@ -14,47 +14,33 @@ class ara_user_information(ara_forms):
         "(M)onitor connected users",
         "(Q)uery user",
     ]
+    menudesc = [
+        "Show all registered users\n",
+        "Show information about\ncurrent users",
+        "Monitor current users\n",
+        "Query about user's last used\nIP, introduction, etc.",
+    ]
     def __keypress__(self, size, key):
-        key = key.strip().lower()
-        if key == "tab":
-            if self.maincolumn.get_focus() == self.menulist:
-                self.maincolumn.set_focus(self.bests)
-            elif self.maincolumn.get_focus() == self.bests:
-                self.maincolumn.set_focus(self.menulist)
-        elif key == "enter":
-            if self.maincolumn.get_focus() == self.menulist:
-                pos = self.menulist.get_focus()[1]
-                if pos == 1:
-                    # TODO: 새 글 읽기로 가기
-                    pass
-                elif pos == 2:
-                    ara_list_boards(self.session_key).main()
-        else:
-            self.frame.keypress(size, key)
+        self.frame.keypress(size, key)
 
     def __initwidgets__(self):
 	self.header = urwid.Filler(urwid.Text(u"ARA: User Information", align='center'))
-        menuitems = [urwid.Text('\n')]
-        menuitems +=[Item(" * "+w+"\n", None, 'selected') for w in self.menu]
+        menuitems = [Item(" * "+w+"\n", None, 'selected') for w in self.menu]
         self.menulist = urwid.ListBox(urwid.SimpleListWalker(menuitems))
 
-        self.maincolumn = urwid.Columns([('weight',40,self.menulist),('weight',60,self.bests)])
+        menudescs = [Item(w, None, 'selected') for w in self.menudesc]
+        self.menudesclist = urwid.ListBox(urwid.SimpleListWalker(menudescs))
+
+        self.maincolumn = urwid.Columns([('weight',40,self.menulist),('weight',60,self.menudesclist)])
 
         infotext = urwid.Filler(urwid.Text("  * Use [Tab] or arrow key to move each items"))
 
-        okbutton = urwid.Filler(urwid.Button("OK"))
-        cancelbutton = urwid.Filler(urwid.Button("Cancel"))
-        buttoncolumn = self._make_column(okbutton, cancelbutton, 50, 50)
+        self.okbutton = urwid.Filler(urwid.Button("OK"))
+        self.cancelbutton = urwid.Filler(urwid.Button("Cancel"))
+        self.buttoncolumn = self._make_column(self.okbutton, self.cancelbutton, 50, 50)
 
-        content = [('fixed',1, self.header),('fixed',1,self.dash),self.maincolumn,('fixed',1,self.dash),('fixed',2,self.copyrightnotice)]
+        content = [('fixed',1, self.header),('fixed',1,self.dash),self.maincolumn,('fixed',1,self.dash),('fixed',1,infotext),('fixed',2,self.buttoncolumn)]
         self.mainpile = urwid.Pile(content)
-
-        self.keymap = {
-            "left":"",
-            "right":"",
-            "j":"down",
-            "k":"up",
-            }
 
         return self.mainpile
 
