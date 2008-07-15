b36e76b66fde1fc6762bd7f3706dcc581da98ff9
peremen <devnull@localhost>
2008-07-15 06:16:00 +0000

    Preparing for lsitview. 170은 내꺼

--- tarara/widget.py
+++ tarara/widget.py
@@ -9,7 +9,12 @@ class Selectable:
     def keypress(self, size, key):
         return key
 
-class MarkerSelect(Selectable, urwid.WidgetWrap):
+class Item(Selectable, urwid.AttrWrap):
+    def __init__(self, text, attr, focus_attr=None):
+        w = urwid.Text(text)
+        urwid.AttrWrap.__init__(self, w, attr, focus_attr)
+
+class MarkedItem(Selectable, urwid.WidgetWrap):
     def __init__(self, marker, widget):
         width = len(marker)
         self.focus = ('fixed', width, urwid.Text(marker))
@@ -38,7 +43,26 @@ class FieldRow(urwid.WidgetWrap):
                 widgets.append(('fixed', width, widget))
         return urwid.Columns(widgets, dividechars=1)
 
-class Item(Selectable, urwid.AttrWrap):
-    def __init__(self, text, attr, focus_attr=None):
-        w = urwid.Text(text)
-        urwid.AttrWrap.__init__(self, w, attr, focus_attr)
+class RowItem(FieldRow):
+    fields = [
+        ('number', 5, 'right'),
+        ('id', 8, 'left'),
+        ('name', 10, 'left'),
+        ('date', 5, 'right'),
+        ('subject', 0, 'left'),
+    ]
+
+class ListView(object):
+    def make_widget(data):
+        return widget.MarkerSelect('>', RowItem(data))
+
+    def make_header():
+        data = {'number': u'번호', 'id': u'글쓴이', 'name': u'이름', 'date': u'날짜', 'subject': u'제목'}
+        return urwid.AttrWrap(make_widget(data), 'reversed')
+
+    def get_view():
+        walker = urwid.SimpleListWalker([make_widget({'number': '1', 'id': '2', 'name': 'peremen', 'date': 'today', 'subject': 'good article'})])
+
+        body = urwid.ListBox(walker)
+        header = make_header()
+        return urwid.Frame(body, header)
