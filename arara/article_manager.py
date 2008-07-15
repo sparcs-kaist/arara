f88ca8b26182d956e0732477148fddf7846b4629
serialx <devnull@localhost>
2008-07-15 06:16:28 +0000

    ArticleManager 가 글 no를 리턴하도록 수정

--- arara/article_manager.py
+++ arara/article_manager.py
@@ -236,7 +236,9 @@ class ArticleManager(object):
         article_list = []
         try:
             for no in range(len(self.articles[board_name])):
-                article_list.append(self.articles[board_name][no+1])
+                article = self.articles[board_name][no+1]
+                article['no'] = no + 1
+                article_list.append(article)
             return True, article_list 
         except KeyError: 
             return False, 'BOARD_NOT_EXIST'
