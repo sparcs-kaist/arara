a9cb81fe5060b4cc1de3a9cbc5f84e6b6f3c13d5
serialx <devnull@localhost>
2008-07-17 07:49:40 +0000

    투베 윅배 기본적인 틀 완성

--- warara/main/views.py
+++ warara/main/views.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
+import datetime
+
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 def index(request):
-    rendered = render_to_string('index.html', {})
+    SAMPLE_BEST = {
+            'todays_best_list': [
+                {'title': '아라가 새로 바뀌었다!', 'date': datetime.datetime.now(), 'reply_count': 213},
+                {'title': '아라 참 멋지네요', 'date': datetime.datetime.now(), 'reply_count':56},
+                {'title': '우왕ㅋ국ㅋ', 'date': datetime.datetime.now(), 'reply_count':44},
+                ],
+            'weekly_best_list': [
+                {'title': '아라가 새로 바뀌었다!', 'date': datetime.datetime.now(), 'reply_count': 213},
+                {'title': '아라 참 멋지네요', 'date': datetime.datetime.now(), 'reply_count':56},
+                {'title': '우왕ㅋ국ㅋ', 'date': datetime.datetime.now(), 'reply_count':44},
+                ],
+            }
+    rendered = render_to_string('index.html', SAMPLE_BEST)
     return HttpResponse(rendered)
