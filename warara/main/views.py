27095b368d4e04e1d59cf677ce3cc59acd940698
serialx <devnull@localhost>
2008-07-17 07:27:01 +0000

    기본 웹페이지가 나오도록 수정

--- warara/main/views.py
+++ warara/main/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
+def index(request):
+    rendered = render_to_string('index.html', {})
+    return HttpResponse(rendered)
