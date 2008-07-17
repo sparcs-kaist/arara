706f227c3949a9b501b98a261bb4e0b0af2fd8bf
serialx <devnull@localhost>
2008-07-17 07:16:51 +0000

    urls.py 파일 추가

--- warara/urls.py
+++ warara/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^warara/', include('warara.foo.urls')),
+    (r'^account/', include('warara.account.urls')),
+    (r'^blacklist/', include('warara.blacklist.urls')),
+    (r'^board/', include('warara.board.urls')),
+    (r'^message/', include('warara.message.urls')),
 
     # Uncomment this for admin:
 #     (r'^admin/', include('django.contrib.admin.urls')),
