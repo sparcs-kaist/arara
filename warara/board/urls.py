9b3d3547e9425025821b097bfab0346be2b57f9a
serialx <devnull@localhost>
2008-07-17 08:29:01 +0000

    Board 쪽 기본적인 url 구조 만듬.

--- warara/board/urls.py
+++ warara/board/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
+    ('^$', 'warara.board.views.index'),
+    ('^(?P<board_name>[a-z]+)/$', 'warara.board.views.list'),
 )
--- warara/board/views.py
+++ warara/board/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
+
+def index(request):
+    rendered = render_to_string('board/index.html', {})
+    return HttpResponse(rendered)
+
+
+def list(request, board_name):
+    rendered = render_to_string('board/list.html', {})
+    return HttpResponse(rendered)
