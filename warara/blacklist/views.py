2f9140a799485cfcc6caad2949172bac8b7fb5ff
jacob <devnull@localhost>
2008-07-18 07:40:58 +0000

    blacklist can be operated

--- warara/blacklist/views.py
+++ warara/blacklist/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+
+#import arara
+
+def blacklist(request):
+    rendered = render_to_string('blacklist/blacklist_frame.html')
+    return HttpResponse(rendered)
