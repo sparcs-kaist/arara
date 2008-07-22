e68f3f393432deb0657935b2a778c223c290a00a
serialx <devnull@localhost>
2008-07-22 07:41:01 +0000

    Blacklist 페이지 대부분의 기능 작동하도록 수정 (with jacob)

--- warara/blacklist/urls.py
+++ warara/blacklist/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^add$', 'warara.blacklist.views.add'),
+    (r'^add/$', 'warara.blacklist.views.add'),
+    (r'^update/$', 'warara.blacklist.views.update'),
+    (r'^delete/$', 'warara.blacklist.views.delete'),
     (r'^$', 'warara.blacklist.views.index'),
 )
--- warara/blacklist/views.py
+++ warara/blacklist/views.py
@@ -1,5 +1,5 @@
 from django.template.loader import render_to_string
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 import arara
 
@@ -21,10 +21,58 @@ def add(request):
         server = arara.get_server()
         sess = test_login()
         ret, message = server.blacklist_manager.add(sess, blacklist_id)
-        return HttpResponse(message)
+        assert ret, message
+        return HttpResponseRedirect("/blacklist/")
     else:
         return HttpResponse('Must use POST')
 
+def delete(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        server = arara.get_server()
+        sess = test_login()
+        ret, message = server.blacklist_manager.delete(sess, username)
+        assert ret, message
+        return HttpResponseRedirect("/blacklist/")
+
+
+def update(request):
+    server = arara.get_server()
+    sess = test_login()
+    ret, blacklist = server.blacklist_manager.list(sess)
+    for b in blacklist:
+        article_bl_key = 'blacklist_article_%s' % b['username']
+        if article_bl_key in request.POST:
+            b['article'] = True
+        else:
+            b['article'] = False
+        message_bl_key = 'blacklist_message_%s' % b['username']
+        if message_bl_key in request.POST:
+            b['message'] = True
+        else:
+            b['message'] = False
+
+        server.blacklist_manager.modify(sess, b)
+
+    return HttpResponse(repr(request))
+
+
 def index(request):
-    rendered = render_to_string('blacklist/index.html')
+    server = arara.get_server()
+    sess = test_login()
+    r = {}
+    ret, blacklist = server.blacklist_manager.list(sess)
+    r['blacklist'] = blacklist
+
+    if 'search' in request.GET:
+        query = request.GET['search']
+        search_type = 'username'
+        if 'search_type' in request.GET:
+            search_type = request.GET['search_type']
+        search_user_info = {search_type: query}
+        ret, user_id = server.member_manager.search_user(sess, search_user_info)
+        if ret:
+            r['search_result'] = user_id
+
+    rendered = render_to_string('blacklist/index.html', r)
     return HttpResponse(rendered)
