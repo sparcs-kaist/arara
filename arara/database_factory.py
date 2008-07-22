02e68f6eb8d60412ba4382cb2a056153b681a930
pipoket <devnull@localhost>
2008-07-22 16:17:07 +0000

    Removed old memory based arara.

--- arara/database_factory.py
+++
@@ -1,9 +0,0 @@
-
-conn = None
-def get_database_connection():
-    '''Factory method to create database connection.'''
-    global conn
-    if not conn:
-        import sqlite3
-        conn = sqlite3.connect(':memory:')
-    return conn
