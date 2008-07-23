67b1b2606ab430c0a453297e5c420900ab90cbf8
serialx <devnull@localhost>
2008-07-23 06:14:09 +0000

    j/k 키로 게시물 위 아래로 왔다갔다 하는 기능 추가.

---
+++ warara/media/js/board_list.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var cursor_pos = 1;
+    var row_count = $("#article_table tr").length - 1;
+    update_table();
+    function update_table() {
+        $("#article_table tr").removeClass("row_highlight");
+        $("#article_table tr").eq(cursor_pos).addClass("row_highlight");
+    }
+    function read_article() {
+        var article_link = $("#article_table tr").eq(cursor_pos).children(".title_col").children("a").attr("href");
+        location.href = article_link;
+    }
+    $(document).keydown(function(event) {
+        switch (event.keyCode) {
+            case 13:  // enter
+                read_article();
+                break;
+            case 74:  // down
+                cursor_pos += 1;
+                if (cursor_pos > row_count) cursor_pos = row_count;
+                update_table();
+                break;
+            case 75:  // up
+                cursor_pos -= 1;
+                if (cursor_pos < 1) cursor_pos = 1;
+                update_table();
+                break;
+        }
+    });
+});
