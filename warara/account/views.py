28a6b0741087940d346e7de05d5a453a595d0230
jacob <devnull@localhost>
2008-07-23 09:47:24 +0000

    account added

--- warara/account/views.py
+++ warara/account/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpResponseRedirect
+
+#import arara
+
+def index(request):
+    rendered = render_to_string('account/index.html')
+    return HttpResponse(rendered)
