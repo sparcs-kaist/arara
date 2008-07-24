bb1908c697f0b23955bf6a5e6402b096dcb26d8a
serialx <devnull@localhost>
2008-07-24 13:09:06 +0000

    아라라 서버 코드 리팩토링.

--- bin/arara_server.py
+++ bin/arara_server.py
@@ -14,26 +14,27 @@ from thirdparty import wsgiserver
 
 from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
 
+dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
+arara.model.init_database()
+namespace = arara.get_namespace()
+dispatcher.register_instance(namespace, allow_dotted_names=True)
+    
+def arara_app(environ, start_response):
+    status = '200 OK'
+    response_headers = [('Content-type','text/xml')]
+    start_response(status, response_headers)
+
+    if environ['REQUEST_METHOD'] == 'POST':
+        raw_post_data = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH')))
+        result = dispatcher._marshaled_dispatch(raw_post_data)
+        return result
+
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     parser.add_option("-p", "--port=", dest="port", default=8000, type="int",
                       help="The port to bind")
     options, args = parser.parse_args()
     print "Opening ARAra XMLRPC middleware on port", options.port, "..."
-    dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
-    arara.model.init_database()
-    namespace = arara.get_namespace()
-    dispatcher.register_instance(namespace, allow_dotted_names=True)
-    
-    def arara_app(environ, start_response):
-        status = '200 OK'
-        response_headers = [('Content-type','text/xml')]
-        start_response(status, response_headers)
-
-        if environ['REQUEST_METHOD'] == 'POST':
-            raw_post_data = environ['wsgi.input'].read(int(environ.get('CONTENT_LENGTH')))
-            result = dispatcher._marshaled_dispatch(raw_post_data)
-            return result
     
     server = wsgiserver.CherryPyWSGIServer(
                 ('0.0.0.0', options.port), arara_app,
