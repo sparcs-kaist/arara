771f48c22413e0c4c5d19382aa4d632f8669b2fd
jacob <devnull@localhost>
2008-07-30 05:33:47 +0000

    *The field is empty*, register caution, dont appear when press tab key to pass the textarea

--- warara/media/js/account.js
+++ warara/media/js/account.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
-    $("input.required_field").keyup(function(event) {
-        if (!$(this).val()) {
-            $(this).parent().children("label").children("span.feedback")
-                .text("The field is empty");
-        }
-        else
-            $(this).parent().children("label").children("span.feedback").text("");
-    });
-
-    $("#re_password_field").keyup(function(event) {
-        if (!($("#password_field").val() == $("#re_password_field").val())) {
-            $("#password_field").parent().children("label").children("span.feedback")
-                .text("The password doesn't matched with the re-enter");
-        }
-        else
-            $("#password_field").parent().children("label").children("span.feedback").text("");
+    $("#id").focus();
+    
+    $("input.required_field").focus(function() {
+        $("input.required_field").keyup(function(event) {
+            if (!(event.keyCode == 09)) {
+                if (!$(this).val()) {
+                    $(this).parent().children("label").children("span.feedback").text("The field is empty");
+                }
+                else
+                    $(this).parent().children("label").children("span.feedback").text("");
+            }
+        });
+        $("#re_password_field").keyup(function(event) {
+            if (!($("#password_field").val() == $("#re_password_field").val())) {
+                $("#password_field").parent().children("label").children("span.feedback")
+                    .text("The password doesn't matched with the re-enter");
+            }
+            else
+                $("#password_field").parent().children("label").children("span.feedback").text("");
+        });
+        $(this).parent().children("label").children("span.feedback").text("");
     });
     
     $(".submit").click(function(event) {
