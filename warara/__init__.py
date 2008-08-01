512c369d2aab8783c5677265665a399c8e912fea
ssaljalu <devnull@localhost>
2008-08-01 07:17:47 +0000

    첫 화면에 투베, 윅베 API 연동

--- warara/__init__.py
+++ warara/__init__.py
@@ -2,11 +2,13 @@
 def check_logged_in(request):
     r = {}
     if "arara_session_key" in request.session:
-        sess = request.session["arara_session_key"]
+        sess = request.session['arara_session_key']
         r['logged_in'] = True
+        r['username'] = request.session['arara_username']
     else:
         sess = ""
         r['logged_in'] = False
+        r['id'] = ""
 
     return sess, r
 
--- warara/main/views.py
+++ warara/main/views.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 import datetime
+import arara
 import warara
 
 from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 def index(request):
+    server = arara.get_server() 
     sess, r = warara.check_logged_in(request)
-    SAMPLE_BEST = {
-            'todays_best_list': [
-                {'title': '아라가 새로 바뀌었다!', 'date': datetime.datetime.now(), 'reply_count': 213},
-                {'title': '아라 참 멋지네요', 'date': datetime.datetime.now(), 'reply_count':56},
-                {'title': '우왕ㅋ국ㅋ', 'date': datetime.datetime.now(), 'reply_count':44},
-                ],
-            'weekly_best_list': [
-                {'title': '아라가 새로 바뀌었다!', 'date': datetime.datetime.now(), 'reply_count': 213},
-                {'title': '아라 참 멋지네요', 'date': datetime.datetime.now(), 'reply_count':56},
-                {'title': '우왕ㅋ국ㅋ', 'date': datetime.datetime.now(), 'reply_count':44},
-                ],
-            }
-    SAMPLE_BEST['logged_in'] = r['logged_in']
-    rendered = render_to_string('index.html', SAMPLE_BEST)
+    suc, ret = server.article_manager.get_today_best_list(5)
+    assert suc, ret
+    r['todays_best_list'] = ret
+    suc, ret = server.article_manager.get_weekly_best_list(5)
+    assert suc, ret
+    r['weekly_best_list'] = ret
+    rendered = render_to_string('index.html', r)
     return HttpResponse(rendered)
