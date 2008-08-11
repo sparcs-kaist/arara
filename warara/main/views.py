e46c93fd64bd8027faca182d4671936ea046dd63
jacob <devnull@localhost>
2008-08-11 13:49:22 +0000

    best articles interface css was modified, second edition

--- warara/main/views.py
+++ warara/main/views.py
@@ -11,6 +11,10 @@ import arara
 def index(request):
     server = arara.get_server() 
     sess, r = warara.check_logged_in(request)
+    
+    if sess:
+        ret, message = server.member_manager.get_info(sess)
+        r['main_nickname'] = message
 
     SAMPLE_BEST = {
             'todays_best_list': [
@@ -28,11 +32,11 @@ def index(request):
 
     suc, ret = server.article_manager.get_today_best_list(5)
     assert suc, ret
-    r['todays_best_list'] = ret
+    r['todays_best_list'] = enumerate(ret)
     suc, ret = server.article_manager.get_weekly_best_list(5)
     assert suc, ret
-    r['weekly_best_list'] = ret
-    
+    r['weekly_best_list'] = enumerate(ret)
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
