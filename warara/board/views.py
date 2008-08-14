f995fb9232161667f1dd0457cb1db4886c1c673d
breadfish <devnull@localhost>
2008-08-14 00:06:44 +0000

    #글목록페이지 기본적인 디자인 입힘

--- warara/board/views.py
+++ warara/board/views.py
@@ -46,7 +46,7 @@ def get_article_list(request, r, mode):
             article_list[i]['title'] = 'deleted'
             article_list[i]['author_username'] = 'deleted'
 
-        max_length = 15 # max title string length
+        max_length = 100 # max title string length
         if len(article_list[i]['title']) > max_length:
             article_list[i]['title'] = article_list[i]['title'][0:max_length]
             article_list[i]['title'] += "..."
