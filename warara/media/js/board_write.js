4ac59585f19ebfe3cc4b840ce9ff49ede5e444f2
breadfish <devnull@localhost>
2008-08-16 00:54:26 +0000

    #글읽는 페이지 기본적인 디자인 입힘 #미리보기는 안됨 #파일 업로드시 input text에서 지워도 파일이 업로드되는 버그 수정 요망

--- warara/media/js/board_write.js
+++ warara/media/js/board_write.js
@@ -3,11 +3,13 @@ $(document).ready(function() {
 
 	$("input[name='file_input_add']").click(function(){
 		$file_no++;
-		$file_input_append = "<p name=" + $file_no + ">\n<caption>file " + $file_no + " : </caption>\n<input type=\"file\" name=\"file" + $file_no + "\"></input>\n<input type=\"button\" value=\"-\" name=\"file_input_delete\"></input>\n</p>";
-		$("#article_write_file").append($file_input_append);
+		//$file_input_append = "<p name=" + $file_no + ">\n<caption>file " + $file_no + " : </caption>\n<input type=\"file\" name=\"file" + $file_no + "\"></input>\n<input type=\"button\" value=\"-\" name=\"file_input_delete\"></input>\n</p>";
+		$("#file_line_model span.article_write_file_caption").text("file " + $file_no);
+		$("#file_line_model input.file_upload").attr("name", "file"+$file_no);
+		$("#article_write_file").append($("#file_line_model").contents().clone());
 
 		$("input[name='file_input_delete']").click(function(){
-			$(this).parent().remove();
+			$(this).parent().parent().remove();
 			});
 		});
 
