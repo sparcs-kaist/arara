cef5e54a55c07f5ef68ed1d353a11768781b45a9
serialx <devnull@localhost>
2008-08-17 15:16:48 +0000

    Fault 에 자세한 traceback 포함시키도록 함.

--- bin/arara_server.py
+++ bin/arara_server.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import optparse
+import traceback
 
 PROJECT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 sys.path.append(PROJECT_PATH)
@@ -13,7 +14,44 @@ from thirdparty import wsgiserver
 
 from SimpleXMLRPCServer import SimpleXMLRPCDispatcher
 
-dispatcher = SimpleXMLRPCDispatcher(allow_none=False, encoding=None)
+class DetailedFaultXMLRPCDispatcher(SimpleXMLRPCDispatcher):
+    def _marshaled_dispatch(self, data, dispatch_method = None):
+        """Dispatches an XML-RPC method from marshalled (XML) data.
+
+        XML-RPC methods are dispatched from the marshalled (XML) data
+        using the _dispatch method and the result is returned as
+        marshalled data. For backwards compatibility, a dispatch
+        function can be provided as an argument (see comment in
+        SimpleXMLRPCRequestHandler.do_POST) but overriding the
+        existing method through subclassing is the prefered means
+        of changing method dispatch behavior.
+        """
+
+        try:
+            params, method = xmlrpclib.loads(data)
+
+            # generate response
+            if dispatch_method is not None:
+                response = dispatch_method(method, params)
+            else:
+                response = self._dispatch(method, params)
+            # wrap response in a singleton tuple
+            response = (response,)
+            response = xmlrpclib.dumps(response, methodresponse=1,
+                                       allow_none=self.allow_none, encoding=self.encoding)
+        except Fault, fault:
+            response = xmlrpclib.dumps(fault, allow_none=self.allow_none,
+                                       encoding=self.encoding)
+        except:
+            # report exception back to server
+            response = xmlrpclib.dumps(
+                xmlrpclib.Fault(1, "%s:%s\n%s" % (sys.exc_type, sys.exc_value, traceback.format_exc())),
+                encoding=self.encoding, allow_none=self.allow_none,
+                )
+
+        return response
+
+dispatcher = DetailedFaultXMLRPCDispatcher(allow_none=False, encoding=None)
 arara.model.init_database()
 namespace = arara.get_namespace()
 dispatcher.register_instance(namespace, allow_dotted_names=True)
