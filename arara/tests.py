1ca689863336e35db60bc226bd3514006fb8bf50
serialx <devnull@localhost>
2008-08-19 22:45:16 +0000

    Doctest 필요없는 hack 삭제

--- arara/tests.py
+++ arara/tests.py
@@ -4,7 +4,6 @@ import sys
 import os
 
 PROJECT_PATH = os.path.join(os.path.dirname(__file__), '../')
-DOCTEST_PATH = os.path.join(PROJECT_PATH, 'arara/test')
 sys.path.append(PROJECT_PATH)
 
 from arara import article_manager
@@ -20,28 +19,28 @@ from arara import sysop_manager
 
 def suite():
     return unittest.TestSuite([
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'article_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'member_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'blacklist_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'messaging_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'login_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'model.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'notice_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'read_status_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'file_manager.txt')),
-                               doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                                   'board_manager.txt')),
-                               #doctest.DocFileSuite(os.path.join(DOCTEST_PATH,
-                               #    'search_manager.txt')),
+                               doctest.DocFileSuite(
+                                   'test/article_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/member_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/blacklist_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/messaging_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/login_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/model.txt'),
+                               doctest.DocFileSuite(
+                                   'test/notice_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/read_status_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/file_manager.txt'),
+                               doctest.DocFileSuite(
+                                   'test/board_manager.txt'),
+                               #doctest.DocFileSuite(
+                               #    'test/search_manager.txt'),
                                #doctest.DocTestSuite(blacklist_manager),
                                #doctest.DocTestSuite(logging_manager),
                                #doctest.DocTestSuite(log_manager),
