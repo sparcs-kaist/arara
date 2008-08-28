dc66c702ea25911c9b00a39f53608358bfe9f45d
peremen <devnull@localhost>
2008-08-28 07:38:25 +0000

    암호 바꾸는 화면에서 Enter, Tab 키 제대로 동작하게 하기.

--- tarara/change_password.py
+++ tarara/change_password.py
@@ -9,7 +9,25 @@ import widget
 
 class ara_change_password(ara_form):
     def keypress(self, size, key):
-        self.mainpile.keypress(size, key)
+        if key in self.keymap:
+            key = self.keymap[key]
+        mainpilefocus = self.mainpile.get_focus()
+        if key == 'tab':
+            if mainpilefocus == self.pwpile:
+                self.mainpile.set_focus(self.buttoncolumn)
+            elif mainpilefocus == self.buttoncolumn:
+                self.mainpile.set_focus(self.pwpile)
+        elif key == 'enter' and mainpilefocus == self.pwpile:
+            pwpilefocus = self.pwpile.get_focus()
+            if pwpilefocus == self.oldpwcolumn:
+                self.pwpile.set_focus(self.newpwcolumn)
+            elif pwpilefocus == self.newpwcolumn:
+                self.pwpile.set_focus(self.confirmcolumn)
+            elif pwpilefocus == self.confirmcolumn:
+                if self.newpwedit.body.get_edit_text() == self.confirmedit.body.get_edit_text():
+                    self.mainpile.set_focus(self.buttoncolumn)
+        else:
+            self.mainpile.keypress(size, key)
         
     def on_button_clicked(self, button):
         retvalue = None
@@ -31,31 +49,36 @@ class ara_change_password(ara_form):
             self.parent.change_page("user_preferences", {'session_key':self.session_key})
 
     def __initwidgets__(self):
+        self.keymap = {
+            'up': '',
+            'down': '',
+            }
 	self.header = urwid.Filler(urwid.Text("ARA: Change Password", align='center'))
         self.header = urwid.AttrWrap(self.header, 'reversed')
 
         self.oldpwedit = urwid.Filler(widget.PasswordEdit(caption="Old password:", wrap='clip'))
         oldpwdesc = urwid.Filler(urwid.Text("Please enter your\nold password"))
-        oldpwcolumn = widget.EasyColumn(self.oldpwedit, oldpwdesc)
+        self.oldpwcolumn = widget.EasyColumn(self.oldpwedit, oldpwdesc)
 
         self.newpwedit = urwid.Filler(widget.PasswordEdit(caption="New password:", wrap='clip'))
         newpwdesc = urwid.Filler(urwid.Text("Minimum password length\nis 4 characters"))
-        newpwcolumn = widget.EasyColumn(self.newpwedit, newpwdesc)
+        self.newpwcolumn = widget.EasyColumn(self.newpwedit, newpwdesc)
 
         self.confirmedit = urwid.Filler(widget.PasswordEdit(caption="Confirm\nnew password:", wrap='clip'))
         confirmdesc = urwid.Filler(urwid.Text("Re-enter your new\npassword"))
-        confirmcolumn = widget.EasyColumn(self.confirmedit, confirmdesc)
+        self.confirmcolumn = widget.EasyColumn(self.confirmedit, confirmdesc)
 
-        self.pwpile = urwid.Pile([oldpwcolumn, newpwcolumn,confirmcolumn])
+        self.pwpile = urwid.Pile([self.oldpwcolumn, self.newpwcolumn,self.confirmcolumn])
 
         self.okbutton = urwid.Filler(urwid.Button("OK", self.on_button_clicked))
         self.cancelbutton = urwid.Filler(urwid.Button("Cancel", self.on_button_clicked))
-        buttoncolumn = widget.EasyColumn(self.okbutton, self.cancelbutton, 50, 50)
+        self.buttoncolumn = widget.EasyColumn(self.okbutton, self.cancelbutton, 50, 50)
 
         infotext = urwid.Filler(urwid.Text("""  * Press [Enter] to proceed to the next item, [Shift+Enter] - previous item
   * Press [Tab] to directly jump to OK or Cancel button"""))
 
-        content = [('fixed',1,self.header),self.pwpile,('fixed',2,infotext),('fixed',1,widget.blank),('fixed',1,buttoncolumn)]
+        content = [('fixed',1,self.header),self.pwpile,('fixed',2,infotext),
+                ('fixed',1,widget.blank),('fixed',1,self.buttoncolumn)]
         self.mainpile = urwid.Pile(content)
 
 if __name__=="__main__":
