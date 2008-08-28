a74aabe0b4a4b602c7bbad4b5ea5f5ed1fae7ab2
peremen <devnull@localhost>
2008-08-28 07:15:04 +0000

    display 모듈의 이중 할당 전부 삭제.

--- tarara/display.py
+++ tarara/display.py
@@ -43,59 +43,41 @@ class ara_display:
         if pagename == "login":
             self.view = self.ara_login
         if pagename == "join":
-            self.ara_join = ara_join(self, {})
-            self.view = self.ara_join
+            self.view = ara_join(self, {})
         elif pagename == "welcome":
-            self.ara_welcome = ara_welcome(self, session_key = args['session_key'])
-            self.view = self.ara_welcome
+            self.view = ara_welcome(self, session_key = args['session_key'])
         elif pagename == "main":
-            self.ara_main = ara_main(self, session_key = args['session_key'])
-            self.view = self.ara_main
+            self.view = ara_main(self, session_key = args['session_key'])
         elif pagename == "list_boards":
-            self.ara_list_boards = ara_list_boards(self, session_key = args['session_key'])
-            self.view = self.ara_list_boards
+            self.view = ara_list_boards(self, session_key = args['session_key'])
         elif pagename == "list_pm":
-            self.ara_list_pm = ara_list_pm(self, session_key = args['session_key'])
-            self.view = self.ara_list_pm
+            self.view = ara_list_pm(self, session_key = args['session_key'])
         elif pagename == "write_pm":
-            self.ara_write_pm = ara_write_pm(self, session_key = args['session_key'], mode=args['mode'], reply_to = args['reply_to'])
-            self.view = self.ara_write_pm
+            self.view = ara_write_pm(self, session_key = args['session_key'], mode=args['mode'], reply_to = args['reply_to'])
         elif pagename == "user_preferences":
-            self.ara_user_preferences = ara_user_preferences(self, session_key = args['session_key'])
-            self.view = self.ara_user_preferences
+            self.view = ara_user_preferences(self, session_key = args['session_key'])
         elif pagename == "user_information":
-            self.ara_user_information = ara_user_information(self, session_key = args['session_key'])
-            self.view = self.ara_user_information
+            self.view = ara_user_information(self, session_key = args['session_key'])
         elif pagename == "list_article":
-            self.ara_list_article = ara_list_article(self, args['session_key'], args['board_name'])
-            self.view = self.ara_list_article
+            self.view = ara_list_article(self, args['session_key'], args['board_name'])
         elif pagename == "read_article":
-            self.ara_read_article = ara_read_article(self, args['session_key'], args['board_name'], args['article_id'])
-            self.view = self.ara_read_article
+            self.view = ara_read_article(self, args['session_key'], args['board_name'], args['article_id'])
         elif pagename == "post_article":
-            self.ara_post_article = ara_post_article(self, args['session_key'], args['board_name'], args['mode'], args['article_id'])
-            self.view = self.ara_post_article
+            self.view = ara_post_article(self, args['session_key'], args['board_name'], args['mode'], args['article_id'])
         elif pagename == "user_preferences":
-            self.ara_user_preferences = ara_user_preferences(self, args['session_key'])
-            self.view = self.ara_user_preferences
+            self.view = ara_user_preferences(self, args['session_key'])
         elif pagename == "user_information":
-            self.ara_user_information = ara_user_information(self, args['session_key'])
-            self.view = self.ara_user_information
+            self.view = ara_user_information(self, args['session_key'])
         elif pagename == "change_password":
-            self.ara_change_password = ara_change_password(self, args['session_key'])
-            self.view = self.ara_change_password
+            self.view = ara_change_password(self, args['session_key'])
         elif pagename == "blacklist":
-            self.ara_blacklist = ara_blacklist(self, args['session_key'])
-            self.view = self.ara_blacklist
+            self.view = ara_blacklist(self, args['session_key'])
         elif pagename == "sig_intro":
-            self.ara_sig_intro = ara_sig_intro(self, args['session_key'])
-            self.view = self.ara_sig_intro
+            self.view = ara_sig_intro(self, args['session_key'])
         elif pagename == "query_user":
-            self.ara_query_user = ara_query_user(self, args['session_key'], args['default_user'])
-            self.view = self.ara_query_user
+            self.view = ara_query_user(self, args['session_key'], args['default_user'])
         elif pagename == "change_basic_info":
-            self.ara_change_basic_info = ara_change_basic_info(self, args['session_key'])
-            self.view = self.ara_change_basic_info
+            self.view = ara_change_basic_info(self, args['session_key'])
         elif pagename == "list_connected_users":
             self.view = ara_list_connected_users(self, args['session_key'])
 
