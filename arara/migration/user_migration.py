52fa44ce83cded4560150db83230a80f0c1b76b2
pipoket <devnull@localhost>
2009-03-03 03:28:08 +0000

    마이그레이션 코드들 추가

---
+++ arara/migration/user_migration.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+import MySQLdb
+import sys
+import md5
+import time
+
+sys.path.append('../../')
+sys.path.append('../../gen-py/')
+
+from arara import model
+from sqlalchemy.exceptions import InvalidRequestError, IntegrityError
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from arara.model import settings
+
+nickname = set()
+
+LOGGING_FILENAME = 'conflicts.log'
+
+engine = create_engine(settings.DB_CONNECTION_STRING, encoding='utf-8',
+                    convert_unicode=True, assert_unicode=None,
+                    pool_size=50, max_overflow=100, echo=False)
+Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
+# Check mysql first!
+try:
+    file = open(LOGGING_FILENAME, 'w')
+    db = MySQLdb.connect(db='webara2g', user='pipoket', passwd='tnfqkrtm',
+           host='mir.sparcs.org', use_unicode=True, charset='utf8')
+    cursor = db.cursor()
+    cursor.execute("select * from users where id='combacsa'")
+    count = cursor.rowcount
+    for i, row in enumerate(cursor):
+        id = row[0]
+        username = row[1]
+        password = row[2]
+        nickname = username #XXX: Apply username on nickname to prevent duplication.
+        email = row[6]
+
+        # Same e-mail address might exist(don't know why)
+        # Reset the e-mail address to the example.org one.
+        # User should change their e-mail and get the confirmation
+        email = "%s_PLEASE_INPUT_YOUR_EMAIL@example.org" % id
+
+        # Write original nickname to the signature
+        signature = u'--\n'
+        signature += u'Original Nickname: '
+        signature += row[3]
+     
+        print "[%d %%]Processing Username: %s, nickname: %s..." % ((i*100/count*1.0), username, nickname),
+        try:
+            ## Write user to the new database
+            session = Session()
+            new_user = model.User(username, password, nickname, email, signature, '', 'en')
+            # Constructor of model receives the password and HASHES IT.
+            # As the password is ALREADY HASHED, we have to put original password here
+            new_user.password = password
+            session.save(new_user)
+            
+            ## Set the activation code to the new database
+            key = (username + password + nickname).encode('utf-8')
+            activation_code = md5.md5(key).hexdigest()
+            new_activation = model.UserActivation(new_user, activation_code)
+            session.save(new_activation)
+
+            ## If everything is fine, write them to the database
+            session.commit()
+            session.close()
+            print "DONE."
+        except IntegrityError, e:
+            ## IntegrityError should never happen, but if happens, log it
+            import traceback
+            session.close()
+            print "ALREADY ADDED! LOGGING!"
+            time.sleep(2)
+            file.write((u"***ALREADY ADDED*******User %s " % username) + traceback.format_exc())
+        except Exception, e:
+            import traceback
+            session.close()
+            print "OTHER ERROR HAPPENED! LOGGING!"
+            file.write((u"+++ERROR!!++++++++++++User %s " % username) + traceback.format_exc())
+            time.sleep(2)
+
+except KeyboardInterrupt:
+    print "\nTERMINATING!!! TERMINATING!!!"
+    file.close()
