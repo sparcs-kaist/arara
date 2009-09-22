fbed56df2f098280467af523f0da674ab50b800b
pipoket <devnull@localhost>
2009-09-22 13:03:10 +0000

    Messaging Manager Testcode 완성

--- arara/test/messaging_manager_test.py
+++ arara/test/messaging_manager_test.py
@@ -18,7 +18,7 @@ server = None
 # Time is needed for testing file_manager
 import time
 
-class MessagingMangaerTest(unittest.TestCase):
+class MessagingManagerTest(unittest.TestCase):
     def setUp(self):
         global server
         # Common preparation for all tests
@@ -62,8 +62,372 @@ class MessagingMangaerTest(unittest.TestCase):
         register_key = server.member_manager.register(
                 UserRegistration(**user_reg_dic))
         server.member_manager.confirm(u'serialx', unicode(register_key))
-       self.serialx_session_key = server.login_manager.login(
+        self.serialx_session_key = server.login_manager.login(
                 u'serialx', u'serialx', '143.248.234.140')
+        
+        # Register dodo for test
+        user_reg_dic = {'username':u'zzongaly', 'password':u'zzongaly', 
+                'nickname':u'dodo', 'email':u'zzongaly@example.com', 
+                'signature':u'mikkang friend', 'self_introduction':u'i am dodo', 
+                'default_language':u'english' }
+        register_key = server.member_manager.register(
+                UserRegistration(**user_reg_dic))
+        server.member_manager.confirm(u'zzongaly', unicode(register_key))
+        self.zzongaly_session_key = server.login_manager.login(
+                u'zzongaly', u'zzongaly', '143.248.234.140')
+
+    def test_sent_list(self):
+        ret = server.messaging_manager.sent_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 0)
+        self.assertEqual(ret.new_message_count, 0)
+
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello1')
+        ret = server.messaging_manager.sent_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 1)
+        self.assertEqual(ret.new_message_count, 0)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello1')
+        self.assertEqual(msg.read_status, u'N')
+
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello2')
+        ret = server.messaging_manager.sent_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 2)
+        self.assertEqual(ret.new_message_count, 0)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 2)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello2')
+        self.assertEqual(msg.read_status, u'N')
+
+        for i in range(10):
+            server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello10')
+        ret = server.messaging_manager.sent_list(self.serialx_session_key, 1, 10)
+        self.assertEqual(ret.last_page, 2)
+        self.assertEqual(ret.results, 12)
+        self.assertEqual(ret.new_message_count, 0)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 12)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello10')
+        self.assertEqual(msg.read_status, u'N')
+
+        ret = server.messaging_manager.sent_list(self.serialx_session_key, 2, 10)
+        self.assertEqual(ret.last_page, 2)
+        self.assertEqual(ret.results, 12)
+        self.assertEqual(ret.new_message_count, 0)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 2)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello2')
+        self.assertEqual(msg.read_status, u'N')
+
+        try:
+            server.messaging_manager.sent_list(u'strange_session')
+            fail()
+        except NotLoggedIn:
+            pass
+
+    def test_receive_list(self):
+        ret = server.messaging_manager.receive_list(self.mikkang_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 0)
+        self.assertEqual(ret.new_message_count, 0)
+        
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello')
+        ret = server.messaging_manager.receive_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 0)
+        self.assertEqual(ret.new_message_count, 0)
+
+        ret = server.messaging_manager.receive_list(self.mikkang_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 1)
+        self.assertEqual(ret.new_message_count, 1)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello')
+        self.assertEqual(msg.read_status, u'N')
+
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello2')
+        ret = server.messaging_manager.receive_list(self.mikkang_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 2)
+        self.assertEqual(ret.new_message_count, 2)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 2)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello2')
+        self.assertEqual(msg.read_status, u'N')
+
+    def test_send_message_by_username(self):
+        server.messaging_manager.send_message_by_username(self.mikkang_session_key, u'zzongaly', u'hi dodo username test')
+
+        ret = server.messaging_manager.receive_list(self.zzongaly_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 1)
+        self.assertEqual(ret.new_message_count, 1)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'dodo')
+        self.assertEqual(msg.to, u'zzongaly')
+        self.assertEqual(msg.message, u'hi dodo username test')
+        self.assertEqual(msg.read_status, u'N')
+
+    def test_send_message_by_nickname(self):
+        server.messaging_manager.send_message_by_nickname(self.mikkang_session_key, u'dodo', u'hi dodo nickname test')
+
+        ret = server.messaging_manager.receive_list(self.zzongaly_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 1)
+        self.assertEqual(ret.new_message_count, 1)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'dodo')
+        self.assertEqual(msg.to, u'zzongaly')
+        self.assertEqual(msg.message, u'hi dodo nickname test')
+        self.assertEqual(msg.read_status, u'N')
+        pass
+
+    def test_send_message(self):
+        for i in range(100):
+            server.messaging_manager.send_message(self.mikkang_session_key, u'serialx', unicode(i+1))
+
+        msg = server.messaging_manager.read_received_message(self.serialx_session_key, 92)
+        self.assertEqual(msg.id , 92)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'serialx')
+        self.assertEqual(msg.to, u'serialx')
+        self.assertEqual(msg.message, u'92')
+        self.assertEqual(msg.read_status, u'N')
+
+        ret = server.messaging_manager.receive_list(self.serialx_session_key, 1, 10)
+        self.assertEqual(ret.last_page, 10)
+        self.assertEqual(ret.results, 100)
+        self.assertEqual(ret.new_message_count, 99)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 100)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'serialx')
+        self.assertEqual(msg.to, u'serialx')
+        self.assertEqual(msg.message, u'100')
+        self.assertEqual(msg.read_status, u'N')
+
+        ret = server.messaging_manager.receive_list(self.serialx_session_key, 10, 10)
+        self.assertEqual(ret.last_page, 10)
+        self.assertEqual(ret.results, 100)
+        self.assertEqual(ret.new_message_count, 99)
+        msg = ret.hit[0]
+        self.assertEqual(msg.id , 10)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'serialx')
+        self.assertEqual(msg.to, u'serialx')
+        self.assertEqual(msg.message, u'10')
+        self.assertEqual(msg.read_status, u'N')
+        msg = ret.hit[9]
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'serialx')
+        self.assertEqual(msg.to, u'serialx')
+        self.assertEqual(msg.message, u'1')
+        self.assertEqual(msg.read_status, u'N')
+
+        try:
+            server.messaging_manager.send_message(u'strange_session', u'mikkang', u'SPAM_SPAM!!!!')
+            fail()
+        except NotLoggedIn:
+            pass
+
+        try:
+            server.messaging_manager.send_message(self.serialx_session_key, u'non_exist', u'SPAM~')
+            fail()
+        except InvalidOperation:
+            pass
+
+    def test_read_received_message(self):
+        for i in range(10):
+            server.messaging_manager.send_message(self.mikkang_session_key, u'serialx', unicode(i+1))
+
+        msg = server.messaging_manager.read_received_message(self.serialx_session_key, 5)
+        self.assertEqual(msg.id , 5)
+        self.assertEqual(msg.from_nickname, u'mikkang')
+        self.assertEqual(msg.from_, u'mikkang')
+        self.assertEqual(msg.to_nickname, u'serialx')
+        self.assertEqual(msg.to, u'serialx')
+        self.assertEqual(msg.message, u'5')
+        self.assertEqual(msg.read_status, u'N')
+
+        try:
+            server.messaging_manager.read_received_message(self.serialx_session_key, 12)
+            fail()
+        except InvalidOperation:
+            pass
+
+        try:
+            server.messaging_manager.read_received_message(u'starnge_session_key', 5)
+            fail()
+        except NotLoggedIn:
+            pass
+
+    def test_read_sent_message(self):
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello1')
+        msg = server.messaging_manager.read_sent_message(self.serialx_session_key, 1)
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello1')
+        self.assertEqual(msg.read_status, u'N')
+
+        server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello2')
+        msg = server.messaging_manager.read_sent_message(self.serialx_session_key, 2)
+        self.assertEqual(msg.id , 2)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello2')
+        self.assertEqual(msg.read_status, u'N')
+
+
+        for i in range(10):
+            server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello10')
+
+        msg = server.messaging_manager.read_sent_message(self.serialx_session_key, 12)
+        self.assertEqual(msg.id , 12)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello10')
+        self.assertEqual(msg.read_status, u'N')
+
+        try:
+            server.messaging_manager.read_sent_message(u'strange_session', 3)
+            fail()
+        except NotLoggedIn:
+            pass
+
+        try:
+            server.messaging_manager.read_sent_message(self.serialx_session_key, 99)
+            fail()
+        except InvalidOperation:
+            pass
+
+    def test_delete_received_message(self):
+        for i in range(10):
+            server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello%s' % (i+1))
+
+        ret = server.messaging_manager.receive_list(self.mikkang_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 10)
+        self.assertEqual(ret.new_message_count, 10)
+        server.messaging_manager.read_received_message(self.mikkang_session_key, 2)
+        msg = server.messaging_manager.read_received_message(self.mikkang_session_key, 1)
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello1')
+        self.assertEqual(msg.read_status, u'N')
+
+        server.messaging_manager.delete_received_message(self.mikkang_session_key, 1)
+        try:
+            server.messaging_manager.read_received_message(self.mikkang_session_key, 1)
+            fail()
+        except InvalidOperation:
+            pass
+
+        ret = server.messaging_manager.receive_list(self.mikkang_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 9)
+        self.assertEqual(ret.new_message_count, 8)
+
+        try:
+            server.messaging_manager.delete_received_message(self.serialx_session_key, 99)
+            fail()
+        except InvalidOperation:
+            pass
+
+        try:
+            server.messaging_manager.delete_received_message(u'starnge_session', 2)
+            fail()
+        except NotLoggedIn:
+            pass
+
+    def test_delete_sent_message(self):
+        for i in range(10):
+            server.messaging_manager.send_message(self.serialx_session_key, u'mikkang', u'hello%s' % (i+1))
+
+        ret = server.messaging_manager.sent_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 10)
+        self.assertEqual(ret.new_message_count, 0)
+        msg = server.messaging_manager.read_sent_message(self.serialx_session_key, 1)
+        self.assertEqual(msg.id , 1)
+        self.assertEqual(msg.from_nickname, u'serialx')
+        self.assertEqual(msg.from_, u'serialx')
+        self.assertEqual(msg.to_nickname, u'mikkang')
+        self.assertEqual(msg.to, u'mikkang')
+        self.assertEqual(msg.message, u'hello1')
+        self.assertEqual(msg.read_status, u'N')
+
+        server.messaging_manager.delete_sent_message(self.serialx_session_key, 1)
+        try:
+            server.messaging_manager.read_sent_message(self.serialx_session_key, 1)
+            fail()
+        except InvalidOperation:
+            pass
+
+        ret = server.messaging_manager.sent_list(self.serialx_session_key)
+        self.assertEqual(ret.last_page, 1)
+        self.assertEqual(ret.results, 9)
+        self.assertEqual(ret.new_message_count, 0)
+
+        try:
+            server.messaging_manager.delete_sent_message(self.serialx_session_key, 99)
+            fail()
+        except InvalidOperation:
+            pass
+
+        try:
+            server.messaging_manager.delete_sent_message(u'starnge_session', 2)
+            fail()
+        except NotLoggedIn:
+            pass
 
     def tearDown(self):
         arara.model.clear_test_database()
@@ -71,7 +435,7 @@ class MessagingMangaerTest(unittest.TestCase):
         time.time = self.org_time
 
 def suite():
-    return unittest.TestLoader().loadTestsFromTestCase(FileManagerTest)
+    return unittest.TestLoader().loadTestsFromTestCase(MessagingManagerTest)
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=2).run(suite())
