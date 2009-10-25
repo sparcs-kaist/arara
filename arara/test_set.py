b8593c77f7069b675bb197b6e25ff013546c9331
combacsa <devnull@localhost>
2009-10-25 07:13:54 +0000

    test_set.py now added. it runs all unittest_based tests.

---
+++ arara/test_set.py
@@ -0,0 +1,26 @@
+import unittest
+import test
+
+import test.article_manager_test
+import test.blacklist_manager_test
+import test.board_manager_test
+import test.file_manager_test
+import test.login_manager_test
+import test.member_manager_test
+import test.messaging_manager_test
+import test.model_test
+
+def suite():
+    return unittest.TestSuite([
+        test.article_manager_test.suite(),
+        test.blacklist_manager_test.suite(),
+        test.board_manager_test.suite(),
+        test.file_manager_test.suite(),
+        test.login_manager_test.suite(),
+        test.member_manager_test.suite(),
+        test.messaging_manager_test.suite(),
+        test.model_test.suite()
+        ])
+
+if __name__ == "__main__":
+    unittest.TextTestRunner(verbosity=1).run(suite())
