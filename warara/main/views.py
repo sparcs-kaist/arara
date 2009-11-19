324d6d1f900e796a275791c5f4a4eff33136c26e
daybreaker <devnull@localhost>
2009-11-19 15:00:05 +0000

    warara 첫번째 커밋: 투베의 cache key 오타 수정 및 코드 의미 명확화

--- warara/main/views.py
+++ warara/main/views.py
@@ -24,48 +24,50 @@ def index(request):
 @warara.cache_page(60)
 def main(request):
     server = arara.get_server() 
-    sess, r = warara.check_logged_in(request)
+    sess, ctx = warara.check_logged_in(request)
+    # TODO: rename all 'r' variables to 'ctx' that means 'context'.
 
-    max_length = 45 #todays, weekly best max string length
+    # Get the today best list
     ret = cache.get('today_best_list')
     if not ret:
         ret = server.article_manager.get_today_best_list(5)
         for item in ret:
             item.date = datetime.datetime.fromtimestamp(item.date)
-        cache.set('todays_best_list', ret, 60)
-    r['todays_best_list'] = enumerate(ret)
+        cache.set('today_best_list', ret, 60)
+    ctx['today_best_list'] = enumerate(ret)
+
+    # Get the weekly-best list
     ret = cache.get('weekly_best_list')
     if not ret:
         ret = server.article_manager.get_weekly_best_list(5)
         for item in ret:
             item.date = datetime.datetime.fromtimestamp(item.date)
         cache.set('weekly_best_list', ret, 60)
-    r['weekly_best_list'] = enumerate(ret)
+    ctx['weekly_best_list'] = enumerate(ret)
 
-    #message_check
-    if r['logged_in']:
+    # Get messages for the current user
+    if ctx['logged_in']:
         message_result = server.messaging_manager.receive_list(sess, 1, 1);
-
         if message_result.new_message_count:
-            r['new_message'] = True
+            ctx['new_message'] = True
         else:
-            r['new_message'] = False
+            ctx['new_message'] = False
 
-    rendered = render_to_string('main.html', r)
+    rendered = render_to_string('main.html', ctx)
     return HttpResponse(rendered)
 
 @warara.wrap_error
 def help(request):
     server = arara.get_server() 
-    sess, r = warara.check_logged_in(request)
+    sess, ctx = warara.check_logged_in(request)
     
-    rendered = render_to_string('help.html', r)
+    rendered = render_to_string('help.html', ctx)
     return HttpResponse(rendered)
 
 @warara.wrap_error
 def get_user_info(request):
     if request.method == 'POST':
-        session_key, r = warara.check_logged_in(request)
+        session_key, ctx = warara.check_logged_in(request)
         server = arara.get_server()
         query_user_name = request.POST['query_user_name']
         information = server.member_manager.query_by_username(session_key, query_user_name)
