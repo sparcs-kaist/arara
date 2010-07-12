89ee67994d311dcc5cb48ea7971939a16077ee60
Byun, Kyuhong <combacsa@sparcs.kaist.ac.kr>
2010-07-12 12:47:34 +0900

    실수로 커밋한 e90fbb6a5659 를 롤백.

--- arara/test/bot_manager_test.py
+++ arara/test/bot_manager_test.py
@@ -28,13 +28,13 @@ def stub_start_new_thread(function, argument):
     pass
 
 class BotManagerTest(unittest.TestCase):
-    def _get_user_reg_dic(self, id, campus):
+    def _get_user_reg_dic(self, id):
         return {'username':id, 'password':id, 'nickname':id, 'email':id + u'@example.com',
-                'signature':id, 'self_introduction':id, 'default_language':u'english', 'campus':campus}
+                'signature':id, 'self_introduction':id, 'default_language':u'english', 'campus':u''}
 
-    def _register_user(self, id, campus):
+    def _register_user(self, id):
         # Register a user, log-in, and then return its session_key
-        user_reg_dic = self._get_user_reg_dic(id, campus)
+        user_reg_dic = self._get_user_reg_dic(id)
         register_key = self.engine.member_manager.register_(UserRegistration(**user_reg_dic))
         self.engine.member_manager.confirm(id, unicode(register_key))
         return self.engine.login_manager.login(id, id, u'143.248.234.140')
@@ -58,8 +58,7 @@ class BotManagerTest(unittest.TestCase):
         self.engine = arara_engine.ARAraEngine()
 
         self.session_key_sysop = self.engine.login_manager.login(u'SYSOP', u'SYSOP', u'123.123.123.123.')
-        self.session_key_mikkang = self._register_user(u'mikkang', u'seoul')
-
+        self.session_key_mikkang = self._register_user(u'mikkang')
     def testInit(self):
         # 각각의 Bot의 Instance들을 제대로 생성했는지 검사
         if not('weather' in BOT_SERVICE_LIST and self.engine.bot_manager.weather_bot):
@@ -73,7 +72,7 @@ class BotManagerTest(unittest.TestCase):
         self.assertEqual(u'melong. this is time', recent_article[0].title)
         self.assertEqual(u'melong. this is xml', recent_article[0].content)
 
-    def testRefreshWeatherInfo(self):
+    def testRefrshWeatherInfo(self):
         # refresh_weather_info가 정보를 잘 갱신하는지 검사하는 테스트이다
         def new_stub_toprettyxml(url):
             return u'melong. this is self-refreshed xml'
@@ -91,50 +90,6 @@ class BotManagerTest(unittest.TestCase):
         recent_article = self.engine.article_manager.read_recent_article(self.session_key_sysop, BOT_SERVICE_SETTING['weather_board_name'])
         self.assertEqual(u'melong. this is time', recent_article[0].title)
         self.assertEqual(u'melong. this is self-refreshed xml', recent_article[0].content)
-
-    def testGetWeatherInfo(self):
-        # get_weather_info가 정보를 잘 받아오는지 검사하는 테스트이다.
-        # Initial setting for test this function
-        self.session_key_hodduc = self._register_user(u'hodduc', u'daejeon')
-        self.session_key_sillo = self._register_user(u'sillo', u'')
-        self.engine.bot_manager.refresh_weather_info(self.session_key_sysop)
-
-        def stub_parseString(string):
-            f = open(os.path.join(os.path.dirname(__file__), 'bot_manager_test_weather_info.xml'), 'r')
-            ret = xml.dom.minidom.parse(f)
-            f.close()
-            return ret
-        self.org_parseString = stub_parseString
-        xml.dom.minidom.parseString = stub_parseString
-
-        # Session Key가 비었을 때 빈 인스턴스를 잘 들고 오는가?
-        result = self.engine.bot_manager.get_weather_info(u"")
-        self.assertEqual(result.city, None)
-
-        # User가 Campus 정보를 입력하지 않았을 때 빈 인스턴스를 잘 들고 오는가?
-        result = self.engine.bot_manager.get_weather_info(self.session_key_sillo)
-        self.assertEqual(result.city, None)
-
-        # Seoul 캠퍼스에 사는 Mikkang 유저에 대한 날씨 정보를 잘 들고 오는가?
-        result = self.engine.bot_manager.get_weather_info(self.session_key_mikkang)
-        self.assertEqual(result.city, 'seoul')
-        self.assertEqual(result.current_temperature, 21)
-        self.assertEqual(result.current_condition, u'Partly Cloudy')
-        self.assertEqual(result.current_icon_url, u'/ig/images/weather/partly_cloudy.gif')
-        self.assertEqual(result.tomorrow_icon_url, u'/ig/images/weather/chance_of_rain.gif')
-        self.assertEqual(result.day_after_tomorrow_icon_url, u'/ig/images/weather/chance_of_storm.gif')
-
-        # Daejeon 캠퍼스에 사는 Hodduc 유저에 대한 날씨 정보를 잘 들고 오는가?
-        result = self.engine.bot_manager.get_weather_info(self.session_key_hodduc)
-        self.assertEqual(result.city, 'daejeon')
-        self.assertEqual(result.current_temperature, 25)
-        self.assertEqual(result.current_condition, u'Mostly Cloudy')
-        self.assertEqual(result.current_icon_url, u'/ig/images/weather/mostly_cloudy.gif')
-        self.assertEqual(result.tomorrow_icon_url, u'/ig/images/weather/chance_of_rain.gif')
-        self.assertEqual(result.day_after_tomorrow_icon_url, u'/ig/images/weather/chance_of_rain.gif')
-
-        # Restore Stub Code
-        xml.dom.minidom.parseString = self.org_parseString
 
     def tearDown(self):
         # Restore Stub Code
