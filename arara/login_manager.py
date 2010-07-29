a22333ea06ee3dffb5d99cab8b7495be20ded828
Byun, Kyuhong <combacsa@sparcs.kaist.ac.kr>
2010-07-30 00:21:20 +0900

    LoginManager 에 다수의 주석 추가.

--- arara/login_manager.py
+++ arara/login_manager.py
@@ -24,6 +24,9 @@ class LoginManager(object):
     '''
     
     def __init__(self, engine):
+        '''
+        @type  engine: ARAraEngine
+        '''
         self.lock_session_dic = thread.allocate_lock()
         self.engine = engine
         self.session_dic = {}
@@ -32,6 +35,10 @@ class LoginManager(object):
         self._create_counter_column()
 
     def _create_counter_column(self):
+        '''
+        일일방문자 / 전체방문자를 저장할 객체를 DB 에 만든다.
+        '''
+        # TODO: Exception 을 발생시키는 것보단 count 등을 쓰는 게 낫지 않을까?
         session = model.Session()
         try:
             visitor = session.query(model.Visitor).one()
@@ -52,6 +59,7 @@ class LoginManager(object):
         @rtype: string
         @return: guest_key
         '''
+        # TODO: guest login 이 필요하지 않다면 그냥 지우도록 하자.
 
         raise InvalidOperation('temporarily disabled')
         #hash = hashlib.md5('guest'+''+datetime.datetime.today().__str__()).hexdigest()
@@ -66,7 +74,7 @@ class LoginManager(object):
         지금까지의 ARAra 총 방문자 수와 오늘 하루 방문자수를 리틴해주는 함수
 
         @rtype: arara_thrift.VisitorCount
-        @return: 방문자수와 하루 방문자수를 포함한 객체
+        @return: 총 방문자수와 하루 방문자수를 포함한 객체
         '''
         # TODO: 사실 가장 예쁘게 하는 방법은 visitor 숫자를 늘리는 메소드를
         #       별도의 thread 로 빼 버리고 이 함수 자신은 순수하게 DB를
@@ -116,8 +124,9 @@ class LoginManager(object):
         @type  user_ip: string
         @param user_ip: User IP
         @rtype: string
-        @return: 로그인한 세션 Key
+        @return: 사용자 Login Session
         '''
+        # TODO: login 함수가 고민할 필요가 없는 Exception 들은 그냥 넘기기 (더 이상 매니저간 통신은 Thrift 를 거치지 않는다)
         username = smart_unicode(username)
         password = smart_unicode(password)
         user_ip = smart_unicode(user_ip)
@@ -147,7 +156,7 @@ class LoginManager(object):
         로그아웃 처리를 담당하는 함수.
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
         '''
 
         try:
@@ -160,7 +169,17 @@ class LoginManager(object):
             raise NotLoggedIn()
 
     def _update_monitor_status(self, session_key, action):
-        '''현재 사용자가 어떤 일을 하고있는지 update 하는 메소드.'''
+        '''
+        현재 사용자가 어떤 일을 하고있는지 update 하는 메소드.
+
+        @type  session_key: string
+        @param session_key: 사용자 Login Session
+        @type  action: string
+        @param action: 사용자가 취하고 있는 동작
+        @rtype: boolean
+        @return: 사용자가 로그인 중이면 True, 아니면 False
+        '''
+        # TODO: 이 함수를 누가 호출하는지 보고, 왜 T/F 가 필요한지 확인하기
         action = smart_unicode(action)  
         with self.lock_session_dic:
             if self.session_dic.has_key(session_key):
@@ -171,6 +190,10 @@ class LoginManager(object):
                 return False
 
     def _check_session_status(self):
+        '''
+        정기적으로 실행되면서 장시간 작업이 없었던 사용자를 Logout 시킨다.
+        '''
+        # TODO: 아래 전체적인 구조 다듬기
         while True:
             logger = logging.getLogger('SESSION CLEANER')
             try:
@@ -200,8 +223,12 @@ class LoginManager(object):
         세션 expire시간을 연장해주는 함수
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
+        @rtype: boolean
+        @return:
+            사용자가 로그인중이면 True, 아니면 False
         '''
+        # TODO: 이 함수는 왜 T/F 를 리턴하는가?
         if self.session_dic.has_key(session_key):
             self.session_dic[session_key]['last_action_time'] = time.time()
             return True
@@ -214,10 +241,11 @@ class LoginManager(object):
         세션 정보를 반환하는 함수
 
         @type  session_key: string
-        @param session_key: User Key
-        @rtype: arara_thrift.Session
+        @param session_key: 사용자 Login Session
+        @rtype: ttypes.Session
         @return: 세션 정보를 가지는 객체
         '''
+        # TODO: filter_dict 함수 등을 쓰도록 변경
         try:
             session_info = self.session_dic[session_key]
             filtered_session_info = {}
@@ -237,10 +265,11 @@ class LoginManager(object):
         세션의 사용자 id 만을 반환하는 함수
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
         @rtype: i32
-        @return: 세션의 사용자 id
+        @return: 해당 세션의 사용자의 internal id
         '''
+        # TODO: check_logged_in 같은 것으로 감싸면 구현이 좀 더 깔끔하지 않을까
         try:
             return self.session_dic[session_key]['id']
         except KeyError:
@@ -252,10 +281,11 @@ class LoginManager(object):
         세션의 사용자 ip 만을 반환하는 함수
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
         @rtype: string
         @return: 세션의 사용자 ip
         '''
+        # TODO: check_logged_in 같은 것으로 감싸면 구현이 좀 더 깔끔하지 않을까
         try:
             return self.session_dic[session_key]['ip']
         except KeyError:
@@ -267,10 +297,11 @@ class LoginManager(object):
         현재 온라인 상태인 사용자를 반환하는 함수
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
         @rtype: list<Session>
         @return: 접속중인 사용자들의 Session List
         '''
+        # TODO: check_logged_in 같은 것으로 감싸면 구현이 좀 더 깔끔하지 않을까
 
         ret = []
         with self.lock_session_dic:
@@ -288,10 +319,11 @@ class LoginManager(object):
         로그인 여부를 체크하는 함수
 
         @type  session_key: string
-        @param session_key: User Key
+        @param session_key: 사용자 Login Session
         @rtype: boolean
         @return: session_key 의 사용자가 로그인 해 있는지 여부
         '''
+        # TODO: is_logged_in 의 결과에 따라 Exception 을 Raise 하는 check_logged_in 을 만들까?
 
         if session_key in self.session_dic:
             return True
