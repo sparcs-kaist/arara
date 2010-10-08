5e29db033eaf987347af17460305493c0cc971c1
Junseong Lee <com0021@gmail.com>
2010-10-08 16:35:42 +0000

    디자인 입히기 과정 중 누락되었던 이미지 하나를 추가로 삽입하고, 로그인 화면에서 ID 부분에 기본적으로 focus가 맞춰지도록 스크립트를 추가

--- warara/media/js/layout.js
+++ warara/media/js/layout.js
@@ -51,6 +51,7 @@ $(document).ready(function(){
 		setTimeout("upgradeBrowser()",1000);
 		$(window).bind("scroll",browserOnTop);
 	}
+	if ($("#araId").length) $("#araId").focus();
 });
 
 function setWidth () {
