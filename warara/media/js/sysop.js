4d36bf300da48863e2e7c67733a07ba0dd9fc460
harry <zmsquf99@gmail.com>
2010-12-01 07:20:28 +0900

    sysop 페이지에서 category 수정 가능하도록 변경.

--- warara/media/js/sysop.js
+++ warara/media/js/sysop.js
@@ -12,7 +12,8 @@ function request_action(action){
         request.data = { orig_board_name : selected_board.board_name, 
                          new_board_name : $("#board_name").val(), 
                          new_alias : $("#alias").val(),
-                         new_board_description : $("#board_description").val() };
+                         new_board_description : $("#board_description").val(),
+                         new_category_name : $("#board_category").val() };
     } else if(action == "change_auth_mode"){
 	request.url = "/sysop/change_auth/";
 	request.data = { orig_board_name : selected_board.board_name,
@@ -42,6 +43,8 @@ function update_list(data){
         location.reload(true);
     }
 
+    document.location.reload();
+
     var action_done = splited_data[0].split("\t")[1];
     if(action_done == "hide" || action_done == "return_hide"){
         $("#board_actions li:nth-child(1) a").html((action_done == "hide" ? "보이기" : "숨기기"));
@@ -62,9 +65,9 @@ function update_list(data){
         board = splited_data[i].split("\t");
 
         if(selected_board != null && board[0] == selected_board.board_name){
-            $("#all_board_list tbody").append('<tr class="' + board[3] + ' selected_board"><td>' + board[0] + "</td><td>" + board[1] + '</td><td>' + board[2] + '</td><td>' + board[4] + '</td><td>' + board[5] + '</td><td>' + board[6] + '</td><td><a href="/board/' + board[0] + '" target="_blank">Link</a></td></tr>');
+            $("#all_board_list tbody").append('<tr class="' + board[3] + ' selected_board"><td>' + board[0] + "</td><td>" + board[1] + '</td><td>' + board[2] + '</td><td>' + board[4] + '</td><td>' + board[5] + '</td><td>' + board[6] + '</td><td>' + board[7] + '</td><td><a href="/board/' + board[0] + '" target="_blank">Link</a></td></tr>');
         } else {
-            $("#all_board_list tbody").append('<tr class="' + board[3] + '"><td>' + board[0] + '</td><td>' + board[1]  + '</td><td>' + board[2] + '</td><td>' + board[4] + '</td><td>' + board[5] + '</td><td>' + board[6] + '</td><td><a href="/board/' + board[0] + '" target="_blank">Link</a></td></tr>');
+            $("#all_board_list tbody").append('<tr class="' + board[3] + '"><td>' + board[0] + '</td><td>' + board[1]  + '</td><td>' + board[2] + '</td><td>' + board[4] + '</td><td>' + board[5] + '</td><td>' + board[6] + '</td><td>' + board[7] + '</td><td><a href="/board/' + board[0] + '" target="_blank">Link</a></td></tr>');
         }
     }
     set_item_action();
@@ -75,7 +78,7 @@ function set_item_action(){
         $("#board_actions").show();
         $("#edit_board").show();
         $("#change_auth").show();
-	$("#add_manager").show();
+        $("#add_manager").show();
 
         var is_hidden = $(this).attr("class").indexOf("hidden_board") != -1;
         var board_name = $(this).children(":nth-child(1)").html();
@@ -83,6 +86,7 @@ function set_item_action(){
         var board_description = $(this).children(":nth-child(3)").html();
         var board_read_level = $(this).children(":nth-child(4)").html();
         var board_write_level = $(this).children(":nth-child(5)").html();
+        var board_category = $(this).children(":nth-child(7)").html();
         $("#board_actions li:nth-child(1) a").html(is_hidden?"보이기":"숨기기");
         // 현재 보드 설정값 가져와 셋팅하기
         $("#board_name").val(board_name);
@@ -90,6 +94,7 @@ function set_item_action(){
         $("#board_description").val(board_description);
         $("#change_read_level").val(board_read_level);
         $("#change_write_level").val(board_write_level);
+        $("#board_category > option[value=\"" + board_category + "\"]").attr("selected", "true");
 
         selected_board = { board_name : board_name, is_hidden : is_hidden };
         
