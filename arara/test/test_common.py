d1e567f0a5ccea78aaf0d07ad03cbec4156bdd71
Junseong Lee <com0021@gmail.com>
2011-07-04 15:37:06 +0900

    공통 테스트 부분을 이용해 smtplib.SMTP를 Mockup object로 가로챔.

--- arara/test/test_common.py
+++ arara/test/test_common.py
@@ -2,16 +2,40 @@ import unittest
 import etc.arara_settings
 import logging
 import arara.model, arara.arara_engine
+import smtplib
+
+# Mockup Object for smtplib.SMTP()
+class SMTPMockup:
+    def __init__(self, debug = False):
+        self.debug = debug
+
+    def connect(self, host="localhost", port=25):
+        if self.debug:
+            print "CONNECT", host, port
+
+    def sendmail(self, from_addr, to_addrs, msg):
+        if self.debug:
+            print "SEND", from_addr, to_addrs, msg
+
+    def quit(self):
+        if self.debug:
+            print "QUIT"
 
 # Common Test Sets for all tests
 class AraraTestBase(unittest.TestCase):
-    def setUp(self, use_bot = False, use_database = True):
+    def setUp(self, use_bot = False, use_database = True, use_mail = False):
         self.use_bot = use_bot
         self.use_database = use_database
+        self.use_mail = use_mail
 
         # Overwrite Bot-related configuration
         self.org_BOT_ENABLED = etc.arara_settings.BOT_ENABLED
         etc.arara_settings.BOT_ENABLED = use_bot
+
+        # Overwrite Mail-Transferring Libraries
+        if not use_mail:
+            self.org_SMTP = smtplib.SMTP
+            smtplib.SMTP = SMTPMockup
 
         # Set Logger
         logging.basicConfig(level=logging.ERROR)
@@ -29,3 +53,6 @@ class AraraTestBase(unittest.TestCase):
             arara.model.clear_test_database()
 
         etc.arara_settings.BOT_ENABLED = self.org_BOT_ENABLED
+
+        if not self.use_mail:
+            smtplib.SMTP = SMTPMockup
