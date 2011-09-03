861484f3634a2de29447ecf966b10a4ab9f4c670
Byun, Kyuhong <combacsa@gmail.com>
2011-09-03 15:19:23 +0900

    warara.warara_middleware.set_server() 함수 도입
     * warara/warara_middleware.py:set_server()
       * get_server() 가 리턴하는 서버를 임의로 설정해버림

--- warara/warara_middleware.py
+++ warara/warara_middleware.py
@@ -4,6 +4,7 @@ from etc.warara_settings import ARARA_SERVER_HOST, ARARA_SERVER_BASE_PORT
 
 server = None
 
+
 def get_server():
     '''
     현재의 Python Interpreter 에서 접속한 백엔드와 교신하게 해 준다.
@@ -13,3 +14,11 @@ def get_server():
     if not server:
         server = Server(ARARA_SERVER_HOST, ARARA_SERVER_BASE_PORT)
     return server
+
+
+def set_server(new_server):
+    '''
+    get_server() 가 리턴하는 서버를 바꿔준다.
+    '''
+    global server
+    server = new_server
