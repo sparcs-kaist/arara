c7b1c8e5be307e38df1cbcfaa3de9806865b7fd3
Byun, Kyuhong <combacsa@gmail.com>
2011-09-07 05:50:19 +0900

    #379 arara.test.test_common.AraraTestBase: 사용자 추가를 돕는 3가지 함수 추가
     * arara/test/test_common.py:AraraTestBase
       * get_user_reg_dic(): 사용자 추가를 위한 dict 반환
       * register_user(): 사용자를 추가하고 confirm key 반환
       * register_and_login(): 사용자를 추가하고 Session Key 반환

--- arara/test/test_common.py
+++ arara/test/test_common.py
@@ -1,9 +1,13 @@
+#-*- coding: utf-8 -*-
 import unittest
 import etc.arara_settings
 import logging
 import arara.model, arara.arara_engine
 import smtplib
 import time
+
+import libs
+from arara_thrift import ttypes
 
 # Mockup Object for smtplib.SMTP()
 class SMTPMockup(object):
@@ -75,3 +79,79 @@ class AraraTestBase(unittest.TestCase):
         # Memcache Prefix Rollback
         if etc.arara_settings.USE_MEMCACHED:
             etc.arara_settings.MEMCACHED_PREFIX = self.org_MEMCACHED_PREFIX
+
+    @classmethod
+    def get_user_reg_dic(cls, username, default_user_reg_dic=None):
+        '''
+        사용자 등록에 사용되는 User Registration Dictionary 를 생성한다.
+
+        @type  username: str
+        @param username: 등록하고자 하는 사용자의 username
+        @type  default_user_reg_dic: dict
+        @param default_user_reg_dic: 사용자 등록 시 지정할 값들
+        @rtype: dict
+        @return: 해당 사용자에 대한 User Registration Dictionary
+        '''
+        default_user_reg_dic = default_user_reg_dic or {}
+
+        # Default User Registration Dictionary
+        username = libs.smart_unicode(username)
+        user_reg_dic = {
+            'username': username,
+            'password': username,
+            'nickname': username,
+            'email': username + u'@kaist.ac.kr',
+            'signature': username,
+            'self_introduction': username,
+            'default_language': u'english',
+            'campus': u'Daejeon'
+        }
+        # 파라메터로 지정된 값들은 덮어쓴다.
+        for key in default_user_reg_dic:
+            user_reg_dic[key] = default_user_reg_dic[key]
+
+        return user_reg_dic
+
+    def register_user(self, username, confirm=True, default_user_reg_dic=None):
+        '''
+        사용자를 등록한다. 별도의 에러 처리가 없으니 사용시 주의.
+        또한 MemberManager 구현에 의존적인 점도 주의.
+
+        @type  username: str
+        @param username: 등록하고자 하는 사용자의 username
+        @type  confirm: bool
+        @param confirm: 사용자를 activate 하는가의 여부 (기본값: True)
+        @type  default_user_reg_dic: dict
+        @param default_user_reg_dic: 사용자 등록 시 지정할 값들 (기본값: {})
+        @rtype: str
+        @return: 해당 사용자에 대한 registration key
+        '''
+        default_user_reg_dic = default_user_reg_dic or {}
+
+        user_reg_dic = self.get_user_reg_dic(username, default_user_reg_dic)
+        register_key = self.engine.member_manager.register_(
+                ttypes.UserRegistration(**user_reg_dic))
+        if confirm:
+            self.engine.member_manager.confirm(username, register_key)
+
+        return register_key
+
+    def register_and_login(self, username, default_user_reg_dic=None,
+            ip="127.0.0.1"):
+        '''
+        사용자를 등록하고, 바로 로그인한다.
+        MemberManager, LoginManager 구현이 바르다는 것을 가정한다.
+
+        @type  username: str
+        @param username: 등록하고자 하는 사용자의 username
+        @type  default_user_reg_dic: dict
+        @param default_user_reg_dic: 사용자 등록 시 지정할 값들 (기본값: {})
+        @rtype: str
+        @return: 사용자 Login Session
+        '''
+        default_user_reg_dic = default_user_reg_dic or {}
+
+        _ = self.register_user(username, True, default_user_reg_dic)
+        password = default_user_reg_dic.get("password", username)
+
+        return self.engine.login_manager.login(username, password, ip)
