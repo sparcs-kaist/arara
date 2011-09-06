bfff5fd15eaf437d31ffbb827855191d9b272851
Byun, Kyuhong <combacsa@gmail.com>
2011-09-07 05:46:54 +0900

    #414 libs.smart_unicode() 함수 도입
     * libs/__init__.py:smart_unicode()
       * arara/util.py 와 arara/model.py 에 있던 함수
       * 주어진 파라메터를 unicode 객체로 변환
       * utf-8 또는 cp949 문자열이 들어오는 것을 가정
     * libs/test_libs.py
       * 테스트 코드 추가

--- libs/__init__.py
+++ libs/__init__.py
@@ -16,3 +16,25 @@ def datetime2timestamp(datetime_):
             + datetime_.microsecond / 1e6)
 
 
+def smart_unicode(string):
+    '''
+    주어진 문자열이 unicode 라면 그대로 리턴, 그렇지 않다면 unicode 로 만듦.
+    해당 문자열이 utf-8 인코딩 또는 cp949 인코딩임을 가정.
+
+    >>> smart_unicode(u'아라')
+    u'\uc544\ub77c'
+    >>> smart_unicode('아라')
+    u'\uc544\ub77c'
+    >>> smart_unicode(u'아라'.encode('cp949'))
+    u'\uc544\ub77c'
+
+    @type string: unicode / str
+    @rtype: unicode
+    '''
+    if isinstance(string, unicode):
+        return string
+    else:
+        try:
+            return unicode(string, 'utf-8')
+        except UnicodeDecodeError:
+            return unicode(string, 'cp949')
--- libs/test_libs.py
+++ libs/test_libs.py
@@ -19,6 +19,16 @@ class LibsTest(unittest.TestCase):
         result = libs.timestamp2datetime(0.0)
         self.assertEqual('1970-01-01 09:00:00', str(result))
 
+    def test_smart_unicode(self):
+        # ABC123가나다, 라는 문자열을 이용하여 검사
+        unicode_string = u'ABC123\uac00\ub098\ub2e4'
+        cp949_string = 'ABC123\xb0\xa1\xb3\xaa\xb4\xd9'
+        utf_8_string = 'ABC123\xea\xb0\x80\xeb\x82\x98\xeb\x8b\xa4'
+        self.assertEqual(unicode_string, libs.smart_unicode(cp949_string))
+        self.assertEqual(unicode_string, libs.smart_unicode(utf_8_string))
+        self.assertEqual(unicode_string, libs.smart_unicode(unicode_string))
+
+
 def suite():
     return unittest.TestLoader().loadTestsFromTestCase(LibsTest)
 
