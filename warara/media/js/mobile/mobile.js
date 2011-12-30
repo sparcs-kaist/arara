a464aa94b964e47172a943d0b52a490065d1b3dd
Junseong Lee <com0021@gmail.com>
2011-12-31 07:13:12 +0900

    ID / PASSWORD 저장 구현

--- warara/media/js/mobile/mobile.js
+++ warara/media/js/mobile/mobile.js
@@ -61,4 +61,41 @@ $(document).ready(function(){
     }
     $('#article_buttons .reply').click(reply_handler);
     $('.re_info .reply').click(reply_handler);
+
+    $('.box form').each(function(){
+        var cookie = document.cookie;
+        if(cookie.length > 0){
+            start_ind = cookie.indexOf('saveID=');
+            if(start_ind != -1){
+                id = cookie.substring(start_ind + 7, cookie.length).split(';')[0];
+                if(id.length > 0){
+                    $('#saveID').attr('checked', 'checked');
+                    $('#id').val(id);
+                }
+            }
+            start_ind = cookie.indexOf('savePWD=');
+            if(start_ind != -1){
+                pw = cookie.substring(start_ind + 8, cookie.length).split(';')[0];
+                if(pw.length > 0){
+                    $('#savePWD').attr('checked', 'checked');
+                    $('#pw').val(pw);
+                }
+            }
+        }
+    });
+
+    $('.box form').submit(function(){
+        var date = new Date(), expireDate = new Date();
+        date.setTime(date.getTime() + 30*24*60*60*1000);
+        expireDate.setTime(date.getTime() - 24*60*60*1000);
+        if($('#saveID').is(':checked'))
+            document.cookie = 'saveID' + '=' + $('#id').val() + "; expires=" + date.toGMTString();
+        else
+            document.cookie = 'saveID' + '=' + "; expires=" + expireDate.toGMTString();
+
+        if($('#savePWD').is(':checked'))
+            document.cookie = 'savePWD' + '=' + $('#pw').val() + "; expires=" + date.toGMTString();
+        else 
+            document.cookie = 'savePWD' + '=' + "; expires=" + expireDate.toGMTString();
+    });
 });
