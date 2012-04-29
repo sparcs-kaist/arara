bdc721ad2248bb511e8f833594c01a9426e4afbb
Junseong Lee <com0021@gmail.com>
2012-04-29 16:59:32 +0900

    DIRECT로 엔진을 로드할 때에 log를 남기지 않던 버그를 수정

--- warara/warara_middleware.py
+++ warara/warara_middleware.py
@@ -3,6 +3,8 @@ from middleware.thrift_middleware import Server
 from etc.warara_settings import ARARA_SERVER_HOST, ARARA_SERVER_BASE_PORT
 from etc import warara_settings
 
+import logging
+
 server = None
 
 
@@ -16,6 +18,17 @@ def get_server():
         if warara_settings.SERVER_TYPE == 'THRIFT':
             server = Server(ARARA_SERVER_HOST, ARARA_SERVER_BASE_PORT)
         elif warara_settings.SERVER_TYPE == 'DIRECT':
+            # Set logger
+            # XXX(hodduc): Thrift server와 arara_settings를 참조하지 않는 방향으로 고치면 더 좋을 듯
+            # bin/thrift_arara_server.py와 middleware/thrift_server.py에서 해당 부분을 분리해낼 필요가 있음
+            from middleware.thrift_server import set_default_log_handlers
+            from etc import arara_settings
+            set_default_log_handlers(arara_settings.ARARA_LOG_PATH,
+                    arara_settings.ARARA_DEBUG_HANDLER_ON,
+                    arara_settings.ARARA_DEBUG_LOG_PATH)
+            logger = logging.getLogger('main')
+
+            # Get Engine
             from arara import arara_engine
             from arara import model
             model.init_database()
