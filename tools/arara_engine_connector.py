71d76d3dd82bb63d82fcfc4b73c6bbe945f69926
Junseong Lee <com0021@gmail.com>
2013-12-04 13:28:39 +0900

    arara_engine_connector now uses warara_middleware
    This makes us possible to use that script even on 'DIRECT' mode.

--- tools/arara_engine_connector.py
+++ tools/arara_engine_connector.py
@@ -6,5 +6,7 @@ ARARA_PATH  = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(THRIFT_PATH)
 sys.path.append(ARARA_PATH)
 
-from arara.server import get_server
+os.environ['DJANGO_SETTINGS_MODULE'] = 'warara.settings'
+
+from warara.warara_middleware import get_server
 from arara_thrift.ttypes import *
